17b8a6e7f038ea7fe36c9e6159f56bce042153cf
helloworlde <hellowoodes@gmail.com>
2020-08-25 10:14:35 +0800

    gRPC LB NameResolver

--- api/src/main/java/io/grpc/EquivalentAddressGroup.java
+++ api/src/main/java/io/grpc/EquivalentAddressGroup.java
@@ -17,6 +17,7 @@
 package io.grpc;
 
 import com.google.common.base.Preconditions;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -27,124 +28,138 @@ import java.util.List;
 
 /**
  * A group of {@link SocketAddress}es that are considered equivalent when channel makes connections.
+ * 一组 SocketAddress，当 Channel 创建连接时，他们是等效的
  *
  * <p>Usually the addresses are addresses resolved from the same host name, and connecting to any of
  * them is equally sufficient. They do have order. An address appears earlier on the list is likely
  * to be tried earlier.
+ * 通常这些地址是解析同一个主机名得到的，连接其中的 任何一个都是等效的，地址是有序的，先添加到集合中的会被先尝试
  */
 @ExperimentalApi("https://github.com/grpc/grpc-java/issues/1770")
 public final class EquivalentAddressGroup {
 
-  /**
-   * The authority to be used when constructing Subchannels for this EquivalentAddressGroup.
-   */
-  @Attr
-  @ExperimentalApi("https://github.com/grpc/grpc-java/issues/6138")
-  public static final Attributes.Key<String> ATTR_AUTHORITY_OVERRIDE =
-      Attributes.Key.create("io.grpc.EquivalentAddressGroup.authorityOverride");
-  private final List<SocketAddress> addrs;
-  private final Attributes attrs;
+    /**
+     * The authority to be used when constructing Subchannels for this EquivalentAddressGroup.
+     * 名称用于在创建 Subchannel 的时候使用
+     */
+    @Attr
+    @ExperimentalApi("https://github.com/grpc/grpc-java/issues/6138")
+    public static final Attributes.Key<String> ATTR_AUTHORITY_OVERRIDE =
+            Attributes.Key.create("io.grpc.EquivalentAddressGroup.authorityOverride");
 
-  /**
-   * {@link SocketAddress} docs say that the addresses are immutable, so we cache the hashCode.
-   */
-  private final int hashCode;
+    private final List<SocketAddress> addrs;
+    private final Attributes attrs;
 
-  /**
-   * List constructor without {@link Attributes}.
-   */
-  public EquivalentAddressGroup(List<SocketAddress> addrs) {
-    this(addrs, Attributes.EMPTY);
-  }
+    /**
+     * {@link SocketAddress} docs say that the addresses are immutable, so we cache the hashCode.
+     * 因为 SocketAddress 的地址是不可变的，所以缓存 hashCode
+     */
+    private final int hashCode;
 
-  /**
-   * List constructor with {@link Attributes}.
-   */
-  public EquivalentAddressGroup(List<SocketAddress> addrs, @Attr Attributes attrs) {
-    Preconditions.checkArgument(!addrs.isEmpty(), "addrs is empty");
-    this.addrs = Collections.unmodifiableList(new ArrayList<>(addrs));
-    this.attrs = Preconditions.checkNotNull(attrs, "attrs");
-    // Attributes may contain mutable objects, which means Attributes' hashCode may change over
-    // time, thus we don't cache Attributes' hashCode.
-    hashCode = this.addrs.hashCode();
-  }
-
-  /**
-   * Singleton constructor without Attributes.
-   */
-  public EquivalentAddressGroup(SocketAddress addr) {
-    this(addr, Attributes.EMPTY);
-  }
-
-  /**
-   * Singleton constructor with Attributes.
-   */
-  public EquivalentAddressGroup(SocketAddress addr, @Attr Attributes attrs) {
-    this(Collections.singletonList(addr), attrs);
-  }
-
-  /**
-   * Returns an immutable list of the addresses.
-   */
-  public List<SocketAddress> getAddresses() {
-    return addrs;
-  }
-
-  /**
-   * Returns the attributes.
-   */
-  @Attr
-  public Attributes getAttributes() {
-    return attrs;
-  }
-
-  @Override
-  public String toString() {
-    // TODO(zpencer): Summarize return value if addr is very large
-    return "[" + addrs + "/" + attrs + "]";
-  }
-
-  @Override
-  public int hashCode() {
-    // Avoids creating an iterator on the underlying array list.
-    return hashCode;
-  }
-
-  /**
-   * Returns true if the given object is also an {@link EquivalentAddressGroup} with an equal
-   * address list and equal attribute values.
-   *
-   * <p>Note that if the attributes include mutable values, it is possible for two objects to be
-   * considered equal at one point in time and not equal at another (due to concurrent mutation of
-   * attribute values).
-   */
-  @Override
-  public boolean equals(Object other) {
-    if (!(other instanceof EquivalentAddressGroup)) {
-      return false;
+    /**
+     * List constructor without {@link Attributes}.
+     * 在没有 Attributes 的情况下构建 EquivalentAddressGroup
+     */
+    public EquivalentAddressGroup(List<SocketAddress> addrs) {
+        this(addrs, Attributes.EMPTY);
     }
-    EquivalentAddressGroup that = (EquivalentAddressGroup) other;
-    if (addrs.size() != that.addrs.size()) {
-      return false;
-    }
-    // Avoids creating an iterator on the underlying array list.
-    for (int i = 0; i < addrs.size(); i++) {
-      if (!addrs.get(i).equals(that.addrs.get(i))) {
-        return false;
-      }
-    }
-    if (!attrs.equals(that.attrs)) {
-      return false;
-    }
-    return true;
-  }
 
-  /**
-   * Annotation for {@link EquivalentAddressGroup}'s attributes. It follows the annotation semantics
-   * defined by {@link Attributes}.
-   */
-  @ExperimentalApi("https://github.com/grpc/grpc-java/issues/4972")
-  @Retention(RetentionPolicy.SOURCE)
-  @Documented
-  public @interface Attr {}
+    /**
+     * List constructor with {@link Attributes}.
+     * 使用地址和属性构建 EquivalentAddressGroup
+     */
+    public EquivalentAddressGroup(List<SocketAddress> addrs, @Attr Attributes attrs) {
+        Preconditions.checkArgument(!addrs.isEmpty(), "addrs is empty");
+        this.addrs = Collections.unmodifiableList(new ArrayList<>(addrs));
+        this.attrs = Preconditions.checkNotNull(attrs, "attrs");
+        // Attributes may contain mutable objects, which means Attributes' hashCode may change over
+        // time, thus we don't cache Attributes' hashCode.
+        // 缓存地址的 hashCode
+        hashCode = this.addrs.hashCode();
+    }
+
+    /**
+     * Singleton constructor without Attributes.
+     * 通过一个地址构建 EquivalentAddressGroup
+     */
+    public EquivalentAddressGroup(SocketAddress addr) {
+        this(addr, Attributes.EMPTY);
+    }
+
+    /**
+     * Singleton constructor with Attributes.
+     * 通过一个地址和属性构建 EquivalentAddressGroup
+     */
+    public EquivalentAddressGroup(SocketAddress addr, @Attr Attributes attrs) {
+        this(Collections.singletonList(addr), attrs);
+    }
+
+    /**
+     * Returns an immutable list of the addresses.
+     */
+    public List<SocketAddress> getAddresses() {
+        return addrs;
+    }
+
+    /**
+     * Returns the attributes.
+     */
+    @Attr
+    public Attributes getAttributes() {
+        return attrs;
+    }
+
+    @Override
+    public String toString() {
+        // TODO(zpencer): Summarize return value if addr is very large
+        return "[" + addrs + "/" + attrs + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        // Avoids creating an iterator on the underlying array list.
+        return hashCode;
+    }
+
+    /**
+     * Returns true if the given object is also an {@link EquivalentAddressGroup} with an equal
+     * address list and equal attribute values.
+     * 如果地址和属性相同，则返回 EquivalentAddressGroup 相等
+     *
+     * <p>Note that if the attributes include mutable values, it is possible for two objects to be
+     * considered equal at one point in time and not equal at another (due to concurrent mutation of
+     * attribute values).
+     * 因为属性中包含可变的值，所以两个对象可能在这一时刻相等，但下一时刻不等
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof EquivalentAddressGroup)) {
+            return false;
+        }
+        EquivalentAddressGroup that = (EquivalentAddressGroup) other;
+        if (addrs.size() != that.addrs.size()) {
+            return false;
+        }
+        // Avoids creating an iterator on the underlying array list.
+        for (int i = 0; i < addrs.size(); i++) {
+            if (!addrs.get(i).equals(that.addrs.get(i))) {
+                return false;
+            }
+        }
+        if (!attrs.equals(that.attrs)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Annotation for {@link EquivalentAddressGroup}'s attributes. It follows the annotation semantics
+     * defined by {@link Attributes}.
+     * EquivalentAddressGroup 属性的注解，遵循 Attributes 定义的注释语义
+     */
+    @ExperimentalApi("https://github.com/grpc/grpc-java/issues/4972")
+    @Retention(RetentionPolicy.SOURCE)
+    @Documented
+    public @interface Attr {
+    }
 }
--- grpclb/src/main/java/io/grpc/grpclb/GrpclbNameResolver.java
+++ grpclb/src/main/java/io/grpc/grpclb/GrpclbNameResolver.java
@@ -23,6 +23,8 @@ import io.grpc.EquivalentAddressGroup;
 import io.grpc.NameResolver;
 import io.grpc.internal.DnsNameResolver;
 import io.grpc.internal.SharedResourceHolder.Resource;
+
+import javax.annotation.Nullable;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
@@ -32,111 +34,142 @@ import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.annotation.Nullable;
 
 /**
  * A DNS-based {@link NameResolver} with gRPC LB specific add-ons for resolving balancer
  * addresses via service records.
+ * 基于 DNS 的 NameResolver，带有 gRPC LB 特定的附件，用于通过服务记录来解析地址
+ * <p>
+ * 将 host 通过 DNS 记录解析为相应的 IP 地址
+ * <p>
+ * $ORIGIN example.com.
+ * server              IN A 10.0.0.11
+ * IN A 10.0.0.12
+ * ; grpclb for server.example.com goes to lb.example.com:1234
+ * _grpclb._tcp.server IN SRV 0 0 1234 lb
+ * ; lb.example.com has 3 IP addresses
+ * lb                  IN A 10.0.0.1
+ * IN A 10.0.0.2
+ * IN A 10.0.0.3
  *
  * @see SecretGrpclbNameResolverProvider
  */
 final class GrpclbNameResolver extends DnsNameResolver {
 
-  private static final Logger logger = Logger.getLogger(GrpclbNameResolver.class.getName());
+    private static final Logger logger = Logger.getLogger(GrpclbNameResolver.class.getName());
 
-  // From https://github.com/grpc/proposal/blob/master/A5-grpclb-in-dns.md
-  private static final String GRPCLB_NAME_PREFIX = "_grpclb._tcp.";
+    // From https://github.com/grpc/proposal/blob/master/A5-grpclb-in-dns.md
+    private static final String GRPCLB_NAME_PREFIX = "_grpclb._tcp.";
 
-  GrpclbNameResolver(
-      @Nullable String nsAuthority,
-      String name,
-      Args args,
-      Resource<Executor> executorResource,
-      Stopwatch stopwatch,
-      boolean isAndroid) {
-    super(nsAuthority, name, args, executorResource, stopwatch, isAndroid);
-  }
-
-  @Override
-  protected InternalResolutionResult doResolve(boolean forceTxt) {
-    List<EquivalentAddressGroup> balancerAddrs = resolveBalancerAddresses();
-    InternalResolutionResult result = super.doResolve(!balancerAddrs.isEmpty());
-    if (!balancerAddrs.isEmpty()) {
-      result.attributes =
-          Attributes.newBuilder()
-              .set(GrpclbConstants.ATTR_LB_ADDRS, balancerAddrs)
-              .build();
+    GrpclbNameResolver(@Nullable String nsAuthority,
+                       String name,
+                       Args args,
+                       Resource<Executor> executorResource,
+                       Stopwatch stopwatch,
+                       boolean isAndroid) {
+        // 调用 DnsNameResolver 进行解析
+        super(nsAuthority, name, args, executorResource, stopwatch, isAndroid);
     }
-    return result;
-  }
 
-  private List<EquivalentAddressGroup> resolveBalancerAddresses() {
-    List<SrvRecord> srvRecords = Collections.emptyList();
-    Exception srvRecordsException = null;
-    ResourceResolver resourceResolver = getResourceResolver();
-    if (resourceResolver != null) {
-      try {
-        srvRecords = resourceResolver.resolveSrv(GRPCLB_NAME_PREFIX + getHost());
-      } catch (Exception e) {
-        srvRecordsException = e;
-      }
-    }
-    List<EquivalentAddressGroup> balancerAddresses = new ArrayList<>(srvRecords.size());
-    Exception balancerAddressesException = null;
-    Level level = Level.WARNING;
-    for (SrvRecord record : srvRecords) {
-      try {
-        // Strip trailing dot for appearance's sake. It _should_ be fine either way, but most
-        // people expect to see it without the dot.
-        String authority = record.host.substring(0, record.host.length() - 1);
-        // But we want to use the trailing dot for the IP lookup. The dot makes the name absolute
-        // instead of relative and so will avoid the search list like that in resolv.conf.
-        List<? extends InetAddress> addrs = addressResolver.resolveAddress(record.host);
-        List<SocketAddress> sockAddrs = new ArrayList<>(addrs.size());
-        for (InetAddress addr : addrs) {
-          sockAddrs.add(new InetSocketAddress(addr, record.port));
+    /**
+     * 解析记录
+     *
+     * @param forceTxt
+     * @return 地址结果
+     */
+    @Override
+    protected InternalResolutionResult doResolve(boolean forceTxt) {
+        // 解析记录
+        List<EquivalentAddressGroup> balancerAddrs = resolveBalancerAddresses();
+        InternalResolutionResult result = super.doResolve(!balancerAddrs.isEmpty());
+
+        if (!balancerAddrs.isEmpty()) {
+            result.attributes = Attributes.newBuilder()
+                                          .set(GrpclbConstants.ATTR_LB_ADDRS, balancerAddrs)
+                                          .build();
         }
-        Attributes attrs =
-            Attributes.newBuilder()
-                .set(GrpclbConstants.ATTR_LB_ADDR_AUTHORITY, authority)
-                .build();
-        balancerAddresses.add(
-            new EquivalentAddressGroup(Collections.unmodifiableList(sockAddrs), attrs));
-      } catch (Exception e) {
-        logger.log(level, "Can't find address for SRV record " + record, e);
-        if (balancerAddressesException == null) {
-          balancerAddressesException = e;
-          level = Level.FINE;
+        return result;
+    }
+
+    /**
+     * 解析记录，将 host 转为 IP 地址集合
+     *
+     * @return 解析的地址
+     */
+    private List<EquivalentAddressGroup> resolveBalancerAddresses() {
+        List<SrvRecord> srvRecords = Collections.emptyList();
+        Exception srvRecordsException = null;
+        // 获取 ResourceResolver 实例
+        ResourceResolver resourceResolver = getResourceResolver();
+        if (resourceResolver != null) {
+            try {
+                // 解析记录
+                srvRecords = resourceResolver.resolveSrv(GRPCLB_NAME_PREFIX + getHost());
+            } catch (Exception e) {
+                srvRecordsException = e;
+            }
         }
-      }
+
+        List<EquivalentAddressGroup> balancerAddresses = new ArrayList<>(srvRecords.size());
+        Exception balancerAddressesException = null;
+        Level level = Level.WARNING;
+        // 将解析的记录转为地址
+        for (SrvRecord record : srvRecords) {
+            try {
+                // Strip trailing dot for appearance's sake. It _should_ be fine either way, but most
+                // people expect to see it without the dot.
+                // 截断最后一个 '.'
+                String authority = record.host.substring(0, record.host.length() - 1);
+                // But we want to use the trailing dot for the IP lookup. The dot makes the name absolute
+                // instead of relative and so will avoid the search list like that in resolv.conf.
+                // 将 host 解析为 IP 集合
+                List<? extends InetAddress> addrs = addressResolver.resolveAddress(record.host);
+                // 遍历添加端口号
+                List<SocketAddress> sockAddrs = new ArrayList<>(addrs.size());
+                for (InetAddress addr : addrs) {
+                    sockAddrs.add(new InetSocketAddress(addr, record.port));
+                }
+                // 添加域名属性
+                Attributes attrs = Attributes.newBuilder()
+                                             .set(GrpclbConstants.ATTR_LB_ADDR_AUTHORITY, authority)
+                                             .build();
+                // 添加到负载均衡的地址中
+                balancerAddresses.add(new EquivalentAddressGroup(Collections.unmodifiableList(sockAddrs), attrs));
+            } catch (Exception e) {
+                logger.log(level, "Can't find address for SRV record " + record, e);
+                if (balancerAddressesException == null) {
+                    balancerAddressesException = e;
+                    level = Level.FINE;
+                }
+            }
+        }
+        if (srvRecordsException != null ||
+                (balancerAddressesException != null && balancerAddresses.isEmpty())) {
+            logger.log(Level.FINE, "Balancer resolution failure", srvRecordsException);
+        }
+        return Collections.unmodifiableList(balancerAddresses);
     }
-    if (srvRecordsException != null
-        || (balancerAddressesException != null && balancerAddresses.isEmpty())) {
-      logger.log(Level.FINE, "Balancer resolution failure", srvRecordsException);
+
+    @VisibleForTesting
+    @Override
+    protected void setAddressResolver(AddressResolver addressResolver) {
+        super.setAddressResolver(addressResolver);
     }
-    return Collections.unmodifiableList(balancerAddresses);
-  }
 
-  @VisibleForTesting
-  @Override
-  protected void setAddressResolver(AddressResolver addressResolver) {
-    super.setAddressResolver(addressResolver);
-  }
+    @VisibleForTesting
+    @Override
+    protected void setResourceResolver(ResourceResolver resourceResolver) {
+        super.setResourceResolver(resourceResolver);
+    }
 
-  @VisibleForTesting
-  @Override
-  protected void setResourceResolver(ResourceResolver resourceResolver) {
-    super.setResourceResolver(resourceResolver);
-  }
+    @VisibleForTesting
+    @Override
+    protected String getHost() {
+        return super.getHost();
+    }
 
-  @VisibleForTesting
-  @Override
-  protected String getHost() {
-    return super.getHost();
-  }
-
-  @VisibleForTesting
-  static void setEnableTxt(boolean enableTxt) {
-    DnsNameResolver.enableTxt = enableTxt;
-  }
+    @VisibleForTesting
+    static void setEnableTxt(boolean enableTxt) {
+        DnsNameResolver.enableTxt = enableTxt;
+    }
 }
