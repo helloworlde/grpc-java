e793b8ee64d24c1f81728b9468d5931519bcd892
helloworlde <hellowoodes@gmail.com>
2020-10-20 09:13:14 +0800

    ClientTransportLifecycleManager

--- netty/src/main/java/io/grpc/netty/ClientTransportLifecycleManager.java
+++ netty/src/main/java/io/grpc/netty/ClientTransportLifecycleManager.java
@@ -19,73 +19,105 @@ package io.grpc.netty;
 import io.grpc.Status;
 import io.grpc.internal.ManagedClientTransport;
 
-/** Maintainer of transport lifecycle status. */
+/**
+ * Maintainer of transport lifecycle status.
+ * Transport 生命周期状态维护
+ */
 final class ClientTransportLifecycleManager {
-  private final ManagedClientTransport.Listener listener;
-  private boolean transportReady;
-  private boolean transportShutdown;
-  private boolean transportInUse;
-  /** null iff !transportShutdown. */
-  private Status shutdownStatus;
-  /** null iff !transportShutdown. */
-  private Throwable shutdownThrowable;
-  private boolean transportTerminated;
 
-  public ClientTransportLifecycleManager(ManagedClientTransport.Listener listener) {
-    this.listener = listener;
-  }
+    private final ManagedClientTransport.Listener listener;
 
-  public void notifyReady() {
-    if (transportReady || transportShutdown) {
-      return;
+    private boolean transportReady;
+    private boolean transportShutdown;
+    private boolean transportInUse;
+
+    /**
+     * null iff !transportShutdown.
+     * 当 Transport 不是 Shutdown 状态时为 null
+     */
+    private Status shutdownStatus;
+    /**
+     * null iff !transportShutdown.
+     * 当 Transport 不是 Shutdown 状态时为 null
+     */
+    private Throwable shutdownThrowable;
+    private boolean transportTerminated;
+
+    public ClientTransportLifecycleManager(ManagedClientTransport.Listener listener) {
+        this.listener = listener;
     }
-    transportReady = true;
-    listener.transportReady();
-  }
 
-  /**
-   * Marks transport as shutdown, but does not set the error status. This must eventually be
-   * followed by a call to notifyShutdown.
-   */
-  public void notifyGracefulShutdown(Status s) {
-    if (transportShutdown) {
-      return;
+    /**
+     * 通知 Transport READY 了
+     */
+    public void notifyReady() {
+        // 如果已经是 Ready 或者处于 SHUTDOWN 状态，则返回
+        if (transportReady || transportShutdown) {
+            return;
+        }
+        transportReady = true;
+        listener.transportReady();
     }
-    transportShutdown = true;
-    listener.transportShutdown(s);
-  }
 
-  public void notifyShutdown(Status s) {
-    notifyGracefulShutdown(s);
-    if (shutdownStatus != null) {
-      return;
+    /**
+     * Marks transport as shutdown, but does not set the error status. This must eventually be
+     * followed by a call to notifyShutdown.
+     * 标记 Transport SHUTDOWN，不设置错误状态，必须在 notifyShutdown 中调用
+     */
+    public void notifyGracefulShutdown(Status s) {
+        if (transportShutdown) {
+            return;
+        }
+        transportShutdown = true;
+        listener.transportShutdown(s);
     }
-    shutdownStatus = s;
-    shutdownThrowable = s.asException();
-  }
 
-  public void notifyInUse(boolean inUse) {
-    if (inUse == transportInUse) {
-      return;
+    /**
+     * 根据所给的状态关闭 Transport
+     */
+    public void notifyShutdown(Status s) {
+        notifyGracefulShutdown(s);
+        if (shutdownStatus != null) {
+            return;
+        }
+        shutdownStatus = s;
+        shutdownThrowable = s.asException();
     }
-    transportInUse = inUse;
-    listener.transportInUse(inUse);
-  }
 
-  public void notifyTerminated(Status s) {
-    if (transportTerminated) {
-      return;
+    /**
+     * 修改使用中状态
+     */
+    public void notifyInUse(boolean inUse) {
+        if (inUse == transportInUse) {
+            return;
+        }
+        transportInUse = inUse;
+        listener.transportInUse(inUse);
     }
-    transportTerminated = true;
-    notifyShutdown(s);
-    listener.transportTerminated();
-  }
 
-  public Status getShutdownStatus() {
-    return shutdownStatus;
-  }
+    /**
+     * 终止 Transport
+     */
+    public void notifyTerminated(Status s) {
+        if (transportTerminated) {
+            return;
+        }
+        transportTerminated = true;
+        notifyShutdown(s);
+        listener.transportTerminated();
+    }
 
-  public Throwable getShutdownThrowable() {
-    return shutdownThrowable;
-  }
+    /**
+     * 获取关闭状态
+     */
+    public Status getShutdownStatus() {
+        return shutdownStatus;
+    }
+
+    /**
+     * 获取关闭时异常
+     */
+    public Throwable getShutdownThrowable() {
+        return shutdownThrowable;
+    }
 }
