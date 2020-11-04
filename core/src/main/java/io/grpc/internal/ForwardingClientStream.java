21c3035bf0f8a50b2e1e3f44defea764c1cba626
helloworlde <hellowoodes@gmail.com>
2020-11-04 09:34:26 +0800

    ForwardingClientStream

--- core/src/main/java/io/grpc/internal/ForwardingClientStream.java
+++ core/src/main/java/io/grpc/internal/ForwardingClientStream.java
@@ -22,103 +22,145 @@ import io.grpc.Compressor;
 import io.grpc.Deadline;
 import io.grpc.DecompressorRegistry;
 import io.grpc.Status;
+
 import java.io.InputStream;
 
 abstract class ForwardingClientStream implements ClientStream {
-  protected abstract ClientStream delegate();
+    /**
+     * 被代理的 ClientStream
+     */
+    protected abstract ClientStream delegate();
 
-  @Override
-  public void request(int numMessages) {
-    delegate().request(numMessages);
-  }
+    /**
+     * 发送指定数量的消息
+     *
+     * @param numMessages the requested number of messages to be delivered to the listener.
+     *                    要传递给监听器的消息的数量
+     */
+    @Override
+    public void request(int numMessages) {
+        delegate().request(numMessages);
+    }
 
-  @Override
-  public void writeMessage(InputStream message) {
-    delegate().writeMessage(message);
-  }
+    /**
+     * 写入消息
+     *
+     * @param message stream containing the serialized message to be sent
+     *                包含要发送的序列化后的消息的流
+     */
+    @Override
+    public void writeMessage(InputStream message) {
+        delegate().writeMessage(message);
+    }
 
-  @Override
-  public void flush() {
-    delegate().flush();
-  }
+    /**
+     * 清空流的缓冲区
+     */
+    @Override
+    public void flush() {
+        delegate().flush();
+    }
 
-  @Override
-  public boolean isReady() {
-    return delegate().isReady();
-  }
+    /**
+     * 返回流是否 ready
+     */
+    @Override
+    public boolean isReady() {
+        return delegate().isReady();
+    }
 
-  @Override
-  public void optimizeForDirectExecutor() {
-    delegate().optimizeForDirectExecutor();
-  }
+    @Override
+    public void optimizeForDirectExecutor() {
+        delegate().optimizeForDirectExecutor();
+    }
 
-  @Override
-  public void setCompressor(Compressor compressor) {
-    delegate().setCompressor(compressor);
-  }
+    @Override
+    public void setCompressor(Compressor compressor) {
+        delegate().setCompressor(compressor);
+    }
 
-  @Override
-  public void setMessageCompression(boolean enable) {
-    delegate().setMessageCompression(enable);
-  }
+    @Override
+    public void setMessageCompression(boolean enable) {
+        delegate().setMessageCompression(enable);
+    }
 
-  @Override
-  public void cancel(Status reason) {
-    delegate().cancel(reason);
-  }
+    /**
+     * 取消流
+     *
+     * @param reason must be non-OK 取消的状态，不能是 ok
+     */
+    @Override
+    public void cancel(Status reason) {
+        delegate().cancel(reason);
+    }
 
-  @Override
-  public void halfClose() {
-    delegate().halfClose();
-  }
+    /**
+     * 半关闭流
+     */
+    @Override
+    public void halfClose() {
+        delegate().halfClose();
+    }
 
-  @Override
-  public void setAuthority(String authority) {
-    delegate().setAuthority(authority);
-  }
+    /**
+     * 设置服务名称
+     */
+    @Override
+    public void setAuthority(String authority) {
+        delegate().setAuthority(authority);
+    }
 
-  @Override
-  public void setFullStreamDecompression(boolean fullStreamDecompression) {
-    delegate().setFullStreamDecompression(fullStreamDecompression);
-  }
+    @Override
+    public void setFullStreamDecompression(boolean fullStreamDecompression) {
+        delegate().setFullStreamDecompression(fullStreamDecompression);
+    }
 
-  @Override
-  public void setDecompressorRegistry(DecompressorRegistry decompressorRegistry) {
-    delegate().setDecompressorRegistry(decompressorRegistry);
-  }
+    @Override
+    public void setDecompressorRegistry(DecompressorRegistry decompressorRegistry) {
+        delegate().setDecompressorRegistry(decompressorRegistry);
+    }
 
-  @Override
-  public void start(ClientStreamListener listener) {
-    delegate().start(listener);
-  }
+    /**
+     * 开始流
+     *
+     * @param listener non-{@code null} listener of stream events
+     *                 流事件监听器
+     */
+    @Override
+    public void start(ClientStreamListener listener) {
+        delegate().start(listener);
+    }
 
-  @Override
-  public void setMaxInboundMessageSize(int maxSize) {
-    delegate().setMaxInboundMessageSize(maxSize);
-  }
+    @Override
+    public void setMaxInboundMessageSize(int maxSize) {
+        delegate().setMaxInboundMessageSize(maxSize);
+    }
 
-  @Override
-  public void setMaxOutboundMessageSize(int maxSize) {
-    delegate().setMaxOutboundMessageSize(maxSize);
-  }
+    @Override
+    public void setMaxOutboundMessageSize(int maxSize) {
+        delegate().setMaxOutboundMessageSize(maxSize);
+    }
 
-  @Override
-  public void setDeadline(Deadline deadline) {
-    delegate().setDeadline(deadline);
-  }
+    /**
+     * 设置过期时间
+     */
+    @Override
+    public void setDeadline(Deadline deadline) {
+        delegate().setDeadline(deadline);
+    }
 
-  @Override
-  public Attributes getAttributes() {
-    return delegate().getAttributes();
-  }
+    @Override
+    public Attributes getAttributes() {
+        return delegate().getAttributes();
+    }
 
-  @Override
-  public String toString() {
-    return MoreObjects.toStringHelper(this).add("delegate", delegate()).toString();
-  }
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this).add("delegate", delegate()).toString();
+    }
 
-  @Override
-  public void appendTimeoutInsight(InsightBuilder insight) {
-    delegate().appendTimeoutInsight(insight);
-  }
+    @Override
+    public void appendTimeoutInsight(InsightBuilder insight) {
+        delegate().appendTimeoutInsight(insight);
+    }
 }
