6a572e46e0f507fad838055462c6f4ce7545361c
helloworlde <hellowoodes@gmail.com>
2020-11-11 10:31:08 +0800

    ManagedChannelProvider

--- api/src/main/java/io/grpc/ManagedChannelProvider.java
+++ api/src/main/java/io/grpc/ManagedChannelProvider.java
@@ -18,107 +18,130 @@ package io.grpc;
 
 import com.google.common.annotations.VisibleForTesting;
 import io.grpc.ServiceProviders.PriorityAccessor;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 /**
  * Provider of managed channels for transport agnostic consumption.
+ * ManagedChannel 的提供者
  *
  * <p>Implementations can be automatically discovered by gRPC via Java's SPI mechanism. For
  * automatic discovery, the implementation must have a zero-argument constructor and include
  * a resource named {@code META-INF/services/io.grpc.ManagedChannelProvider} in their JAR. The
  * file's contents should be the implementation's class name.
+ * 实现可以通过 Java 的 SPI 机制被 gRPC 自动发现，为了实现 SPI，实现类必须要有一个无参的构造器，并且在
+ * Jar 包中包含名为 META-INF/services/io.grpc.ManagedChannelProvider 的文件，文件的内容应当是实现类
+ * 的名称
  *
  * <p>Implementations <em>should not</em> throw. If they do, it may interrupt class loading. If
  * exceptions may reasonably occur for implementation-specific reasons, implementations should
  * generally handle the exception gracefully and return {@code false} from {@link #isAvailable()}.
+ * 实现类不应当抛出异常，如果抛出异常可能会打断类加载，如果特定的实现可能发生合理的异常，应当优雅处理，并通过
+ * isAvailable 方法返回 false
  */
 @Internal
 public abstract class ManagedChannelProvider {
-  @VisibleForTesting
-  static final Iterable<Class<?>> HARDCODED_CLASSES = new HardcodedClasses();
 
-  private static final ManagedChannelProvider provider = ServiceProviders.load(
-      ManagedChannelProvider.class,
-      HARDCODED_CLASSES,
-      ManagedChannelProvider.class.getClassLoader(),
-      new PriorityAccessor<ManagedChannelProvider>() {
-        @Override
-        public boolean isAvailable(ManagedChannelProvider provider) {
-          return provider.isAvailable();
+    /**
+     * 硬编码的类名，返回 ChannelProvider，根据不同的实现可能返回 OkHttp 的 或者 Netty 的
+     */
+    @VisibleForTesting
+    static final Iterable<Class<?>> HARDCODED_CLASSES = new HardcodedClasses();
+
+    /**
+     * 加载 ManagedChannelProvider 的所有可用实现
+     */
+    private static final ManagedChannelProvider provider = ServiceProviders.load(
+            ManagedChannelProvider.class,
+            HARDCODED_CLASSES,
+            ManagedChannelProvider.class.getClassLoader(),
+            new PriorityAccessor<ManagedChannelProvider>() {
+                @Override
+                public boolean isAvailable(ManagedChannelProvider provider) {
+                    return provider.isAvailable();
+                }
+
+                @Override
+                public int getPriority(ManagedChannelProvider provider) {
+                    return provider.priority();
+                }
+            });
+
+    /**
+     * Returns the ClassLoader-wide default channel.
+     * 返回类加载级别的可用的提供器
+     *
+     * @throws ProviderNotFoundException if no provider is available
+     */
+    public static ManagedChannelProvider provider() {
+        if (provider == null) {
+            throw new ProviderNotFoundException("No functional channel service provider found. "
+                    + "Try adding a dependency on the grpc-okhttp, grpc-netty, or grpc-netty-shaded "
+                    + "artifact");
         }
+        return provider;
+    }
 
-        @Override
-        public int getPriority(ManagedChannelProvider provider) {
-          return provider.priority();
+    /**
+     * Whether this provider is available for use, taking the current environment into consideration.
+     * If {@code false}, no other methods are safe to be called.
+     * 提供器是否可用，考虑当前环境，如果是 false，其他的方法不能被安全的调用
+     */
+    protected abstract boolean isAvailable();
+
+    /**
+     * A priority, from 0 to 10 that this provider should be used, taking the current environment into
+     * consideration. 5 should be considered the default, and then tweaked based on environment
+     * detection. A priority of 0 does not imply that the provider wouldn't work; just that it should
+     * be last in line.
+     * Provider 的优先级从 0 到 10，考虑环境，5应当是默认的优先级，根据环境决定，0表示不会工作，会被放在最后
+     */
+    protected abstract int priority();
+
+    /**
+     * Creates a new builder with the given host and port.
+     * 根据所给的名称和端口创建构建器
+     */
+    protected abstract ManagedChannelBuilder<?> builderForAddress(String name, int port);
+
+    /**
+     * Creates a new builder with the given target URI.
+     * 根据所给的 URI 创建构建器
+     */
+    protected abstract ManagedChannelBuilder<?> builderForTarget(String target);
+
+    /**
+     * Thrown when no suitable {@link ManagedChannelProvider} objects can be found.
+     * 当没有可用的 ManagedChannelProvider 对象时抛出
+     */
+    public static final class ProviderNotFoundException extends RuntimeException {
+        private static final long serialVersionUID = 1;
+
+        public ProviderNotFoundException(String msg) {
+            super(msg);
         }
-      });
-
-  /**
-   * Returns the ClassLoader-wide default channel.
-   *
-   * @throws ProviderNotFoundException if no provider is available
-   */
-  public static ManagedChannelProvider provider() {
-    if (provider == null) {
-      throw new ProviderNotFoundException("No functional channel service provider found. "
-          + "Try adding a dependency on the grpc-okhttp, grpc-netty, or grpc-netty-shaded "
-          + "artifact");
     }
-    return provider;
-  }
 
-  /**
-   * Whether this provider is available for use, taking the current environment into consideration.
-   * If {@code false}, no other methods are safe to be called.
-   */
-  protected abstract boolean isAvailable();
-
-  /**
-   * A priority, from 0 to 10 that this provider should be used, taking the current environment into
-   * consideration. 5 should be considered the default, and then tweaked based on environment
-   * detection. A priority of 0 does not imply that the provider wouldn't work; just that it should
-   * be last in line.
-   */
-  protected abstract int priority();
-
-  /**
-   * Creates a new builder with the given host and port.
-   */
-  protected abstract ManagedChannelBuilder<?> builderForAddress(String name, int port);
-
-  /**
-   * Creates a new builder with the given target URI.
-   */
-  protected abstract ManagedChannelBuilder<?> builderForTarget(String target);
-
-  /**
-   * Thrown when no suitable {@link ManagedChannelProvider} objects can be found.
-   */
-  public static final class ProviderNotFoundException extends RuntimeException {
-    private static final long serialVersionUID = 1;
-
-    public ProviderNotFoundException(String msg) {
-      super(msg);
+    /**
+     * 硬编码的类
+     */
+    private static final class HardcodedClasses implements Iterable<Class<?>> {
+        @Override
+        public Iterator<Class<?>> iterator() {
+            List<Class<?>> list = new ArrayList<>();
+            try {
+                list.add(Class.forName("io.grpc.okhttp.OkHttpChannelProvider"));
+            } catch (ClassNotFoundException ex) {
+                // ignore
+            }
+            try {
+                list.add(Class.forName("io.grpc.netty.NettyChannelProvider"));
+            } catch (ClassNotFoundException ex) {
+                // ignore
+            }
+            return list.iterator();
+        }
     }
-  }
-
-  private static final class HardcodedClasses implements Iterable<Class<?>> {
-    @Override
-    public Iterator<Class<?>> iterator() {
-      List<Class<?>> list = new ArrayList<>();
-      try {
-        list.add(Class.forName("io.grpc.okhttp.OkHttpChannelProvider"));
-      } catch (ClassNotFoundException ex) {
-        // ignore
-      }
-      try {
-        list.add(Class.forName("io.grpc.netty.NettyChannelProvider"));
-      } catch (ClassNotFoundException ex) {
-        // ignore
-      }
-      return list.iterator();
-    }
-  }
 }
--- api/src/main/java/io/grpc/ServiceProviders.java
+++ api/src/main/java/io/grpc/ServiceProviders.java
@@ -26,138 +26,154 @@ import java.util.ServiceConfigurationError;
 import java.util.ServiceLoader;
 
 final class ServiceProviders {
-  private ServiceProviders() {
-    // do not instantiate
-  }
-
-  /**
-   * If this is not Android, returns the highest priority implementation of the class via
-   * {@link ServiceLoader}.
-   * If this is Android, returns an instance of the highest priority class in {@code hardcoded}.
-   */
-  public static <T> T load(
-      Class<T> klass,
-      Iterable<Class<?>> hardcoded,
-      ClassLoader cl,
-      PriorityAccessor<T> priorityAccessor) {
-    List<T> candidates = loadAll(klass, hardcoded, cl, priorityAccessor);
-    if (candidates.isEmpty()) {
-      return null;
-    }
-    return candidates.get(0);
-  }
-
-  /**
-   * If this is not Android, returns all available implementations discovered via
-   * {@link ServiceLoader}.
-   * If this is Android, returns all available implementations in {@code hardcoded}.
-   * The list is sorted in descending priority order.
-   */
-  public static <T> List<T> loadAll(
-      Class<T> klass,
-      Iterable<Class<?>> hardcoded,
-      ClassLoader cl,
-      final PriorityAccessor<T> priorityAccessor) {
-    Iterable<T> candidates;
-    if (isAndroid(cl)) {
-      candidates = getCandidatesViaHardCoded(klass, hardcoded);
-    } else {
-      candidates = getCandidatesViaServiceLoader(klass, cl);
-    }
-    List<T> list = new ArrayList<>();
-    for (T current: candidates) {
-      if (!priorityAccessor.isAvailable(current)) {
-        continue;
-      }
-      list.add(current);
+    private ServiceProviders() {
+        // do not instantiate
     }
 
-    // Sort descending based on priority.  If priorities are equal, compare the class names to
-    // get a reliable result.
-    Collections.sort(list, Collections.reverseOrder(new Comparator<T>() {
-      @Override
-      public int compare(T f1, T f2) {
-        int pd = priorityAccessor.getPriority(f1) - priorityAccessor.getPriority(f2);
-        if (pd != 0) {
-          return pd;
+    /**
+     * If this is not Android, returns the highest priority implementation of the class via
+     * {@link ServiceLoader}.
+     * 如果不是 Android 平台，返回最高优先级的 ServiceLoader 实现
+     * If this is Android, returns an instance of the highest priority class in {@code hardcoded}.
+     * 如果是 Android 平台，返回 hardcoded 中优先级最高的实现
+     */
+    public static <T> T load(Class<T> klass,
+                             Iterable<Class<?>> hardcoded,
+                             ClassLoader cl,
+                             PriorityAccessor<T> priorityAccessor) {
+
+        List<T> candidates = loadAll(klass, hardcoded, cl, priorityAccessor);
+
+        if (candidates.isEmpty()) {
+            return null;
         }
-        return f1.getClass().getName().compareTo(f2.getClass().getName());
-      }
-    }));
-    return Collections.unmodifiableList(list);
-  }
-
-  /**
-   * Returns true if the {@link ClassLoader} is for android.
-   */
-  static boolean isAndroid(ClassLoader cl) {
-    try {
-      // Specify a class loader instead of null because we may be running under Robolectric
-      Class.forName("android.app.Application", /*initialize=*/ false, cl);
-      return true;
-    } catch (Exception e) {
-      // If Application isn't loaded, it might as well not be Android.
-      return false;
+        return candidates.get(0);
     }
-  }
-
-  /**
-   * Loads service providers for the {@code klass} service using {@link ServiceLoader}.
-   */
-  @VisibleForTesting
-  public static <T> Iterable<T> getCandidatesViaServiceLoader(Class<T> klass, ClassLoader cl) {
-    Iterable<T> i = ServiceLoader.load(klass, cl);
-    // Attempt to load using the context class loader and ServiceLoader.
-    // This allows frameworks like http://aries.apache.org/modules/spi-fly.html to plug in.
-    if (!i.iterator().hasNext()) {
-      i = ServiceLoader.load(klass);
-    }
-    return i;
-  }
-
-  /**
-   * Load providers from a hard-coded list. This avoids using getResource(), which has performance
-   * problems on Android (see https://github.com/grpc/grpc-java/issues/2037).
-   */
-  @VisibleForTesting
-  static <T> Iterable<T> getCandidatesViaHardCoded(Class<T> klass, Iterable<Class<?>> hardcoded) {
-    List<T> list = new ArrayList<>();
-    for (Class<?> candidate : hardcoded) {
-      list.add(create(klass, candidate));
-    }
-    return list;
-  }
-
-  @VisibleForTesting
-  static <T> T create(Class<T> klass, Class<?> rawClass) {
-    try {
-      return rawClass.asSubclass(klass).getConstructor().newInstance();
-    } catch (Throwable t) {
-      throw new ServiceConfigurationError(
-          String.format("Provider %s could not be instantiated %s", rawClass.getName(), t), t);
-    }
-  }
-
-  /**
-   * An interface that allows us to get priority information about a provider.
-   * 允许获取有关提供器优先级的接口
-   */
-  public interface PriorityAccessor<T> {
-    /**
-     * Checks this provider is available for use, taking the current environment into consideration.
-     * If {@code false}, no other methods are safe to be called.
-     * 检查提供器是否可用，考虑当前环境，如果为 false，没有其他的方法可以安全的调用
-     */
-    boolean isAvailable(T provider);
 
     /**
-     * A priority, from 0 to 10 that this provider should be used, taking the current environment
-     * into consideration. 5 should be considered the default, and then tweaked based on environment
-     * detection. A priority of 0 does not imply that the provider wouldn't work; just that it
-     * should be last in line.
-     * 提供器的优先级，从 0 到 10，考虑当前环境，默认值是 5，然后根据环境检测调整，优先级为 0 不代表不工作，仅代表
-     * 优先级最低
+     * If this is not Android, returns all available implementations discovered via
+     * {@link ServiceLoader}.
+     * If this is Android, returns all available implementations in {@code hardcoded}.
+     * The list is sorted in descending priority order.
+     * 如果不是 Android 平台，返回发现的所有可用的实现
+     * 如果是 Android 平台，返回 hardcode 中所有的可用的实现
+     * 这个集合按照优先级降序排列
      */
-    int getPriority(T provider);
-  }
+    public static <T> List<T> loadAll(Class<T> klass,
+                                      Iterable<Class<?>> hardcoded,
+                                      ClassLoader cl,
+                                      final PriorityAccessor<T> priorityAccessor) {
+        Iterable<T> candidates;
+
+        // 根据不同的平台，构建不同的 Provider 实例
+        if (isAndroid(cl)) {
+            candidates = getCandidatesViaHardCoded(klass, hardcoded);
+        } else {
+            candidates = getCandidatesViaServiceLoader(klass, cl);
+        }
+
+        List<T> list = new ArrayList<>();
+        // 遍历所有的 Provider，将有优先级的加入到集合中
+        for (T current : candidates) {
+            if (!priorityAccessor.isAvailable(current)) {
+                continue;
+            }
+            list.add(current);
+        }
+
+        // Sort descending based on priority.  If priorities are equal, compare the class names to
+        // get a reliable result.
+        // 基于优先级降序排列，如果优先级相同，则比较类名
+        Collections.sort(list, Collections.reverseOrder(new Comparator<T>() {
+            @Override
+            public int compare(T f1, T f2) {
+                int pd = priorityAccessor.getPriority(f1) - priorityAccessor.getPriority(f2);
+                if (pd != 0) {
+                    return pd;
+                }
+                return f1.getClass().getName().compareTo(f2.getClass().getName());
+            }
+        }));
+        return Collections.unmodifiableList(list);
+    }
+
+    /**
+     * Returns true if the {@link ClassLoader} is for android.
+     * 如果 ClassLoader 是用于 Android 的，则返回 true
+     */
+    static boolean isAndroid(ClassLoader cl) {
+        try {
+            // Specify a class loader instead of null because we may be running under Robolectric
+            Class.forName("android.app.Application", /*initialize=*/ false, cl);
+            return true;
+        } catch (Exception e) {
+            // If Application isn't loaded, it might as well not be Android.
+            return false;
+        }
+    }
+
+    /**
+     * Loads service providers for the {@code klass} service using {@link ServiceLoader}.
+     * 通过 ServiceLoader 加载提供器
+     */
+    @VisibleForTesting
+    public static <T> Iterable<T> getCandidatesViaServiceLoader(Class<T> klass, ClassLoader cl) {
+        Iterable<T> i = ServiceLoader.load(klass, cl);
+        // Attempt to load using the context class loader and ServiceLoader.
+        // This allows frameworks like http://aries.apache.org/modules/spi-fly.html to plug in.
+        if (!i.iterator().hasNext()) {
+            i = ServiceLoader.load(klass);
+        }
+        return i;
+    }
+
+    /**
+     * Load providers from a hard-coded list. This avoids using getResource(), which has performance
+     * problems on Android (see https://github.com/grpc/grpc-java/issues/2037).
+     * 加载硬编码的提供器集合，没有使用 getResource 是因为在 Android 有性能问题
+     */
+    @VisibleForTesting
+    static <T> Iterable<T> getCandidatesViaHardCoded(Class<T> klass, Iterable<Class<?>> hardcoded) {
+        List<T> list = new ArrayList<>();
+        // 遍历构建实例
+        for (Class<?> candidate : hardcoded) {
+            list.add(create(klass, candidate));
+        }
+        return list;
+    }
+
+    /**
+     * 根据给定的类构件实例
+     */
+    @VisibleForTesting
+    static <T> T create(Class<T> klass, Class<?> rawClass) {
+        try {
+            return rawClass.asSubclass(klass).getConstructor().newInstance();
+        } catch (Throwable t) {
+            throw new ServiceConfigurationError(String.format("Provider %s could not be instantiated %s", rawClass.getName(), t), t);
+        }
+    }
+
+    /**
+     * An interface that allows us to get priority information about a provider.
+     * 允许获取有关提供器优先级的接口
+     */
+    public interface PriorityAccessor<T> {
+        /**
+         * Checks this provider is available for use, taking the current environment into consideration.
+         * If {@code false}, no other methods are safe to be called.
+         * 检查提供器是否可用，考虑当前环境，如果为 false，没有其他的方法可以安全的调用
+         */
+        boolean isAvailable(T provider);
+
+        /**
+         * A priority, from 0 to 10 that this provider should be used, taking the current environment
+         * into consideration. 5 should be considered the default, and then tweaked based on environment
+         * detection. A priority of 0 does not imply that the provider wouldn't work; just that it
+         * should be last in line.
+         * 提供器的优先级，从 0 到 10，考虑当前环境，默认值是 5，然后根据环境检测调整，优先级为 0 不代表不工作，仅代表
+         * 优先级最低
+         */
+        int getPriority(T provider);
+    }
 }
