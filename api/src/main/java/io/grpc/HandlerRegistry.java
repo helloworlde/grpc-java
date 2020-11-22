10ef87da301aee621ed0e54f0a99cdbea2e81211
helloworlde <hellowoodes@gmail.com>
2020-11-22 19:33:53 +0800

    HandlerRegistry

--- api/src/main/java/io/grpc/HandlerRegistry.java
+++ api/src/main/java/io/grpc/HandlerRegistry.java
@@ -16,47 +16,55 @@
 
 package io.grpc;
 
-import java.util.Collections;
-import java.util.List;
 import javax.annotation.Nullable;
 import javax.annotation.concurrent.ThreadSafe;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Registry of services and their methods used by servers to dispatching incoming calls.
+ * 用于指派处理请求的服务和方法的注册器
  */
 @ThreadSafe
 public abstract class HandlerRegistry {
 
-  /**
-   * Returns the {@link ServerServiceDefinition}s provided by the registry, or an empty list if not
-   * supported by the implementation.
-   */
-  @ExperimentalApi("https://github.com/grpc/grpc-java/issues/2222")
-  public List<ServerServiceDefinition> getServices() {
-    return Collections.emptyList();
-  }
+    /**
+     * Returns the {@link ServerServiceDefinition}s provided by the registry, or an empty list if not
+     * supported by the implementation.
+     * 返回注册的服务的定义集合，如果没有实现则返回空集合
+     */
+    @ExperimentalApi("https://github.com/grpc/grpc-java/issues/2222")
+    public List<ServerServiceDefinition> getServices() {
+        return Collections.emptyList();
+    }
 
-  /**
-   * Lookup a {@link ServerMethodDefinition} by its fully-qualified name.
-   *
-   * @param methodName to lookup {@link ServerMethodDefinition} for.
-   * @param authority the authority for the desired method (to do virtual hosting). If {@code null}
-   *        the first matching method will be returned.
-   * @return the resolved method or {@code null} if no method for that name exists.
-   */
-  @Nullable
-  public abstract ServerMethodDefinition<?, ?> lookupMethod(
-      String methodName, @Nullable String authority);
+    /**
+     * Lookup a {@link ServerMethodDefinition} by its fully-qualified name.
+     * 根据限定全名查找方法的定义
+     *
+     * @param methodName to lookup {@link ServerMethodDefinition} for.
+     *                   要查找的方法名称
+     * @param authority  the authority for the desired method (to do virtual hosting). If {@code null}
+     *                   the first matching method will be returned.
+     *                   服务名，如果为 null，第一个匹配的方法会被返回
+     * @return the resolved method or {@code null} if no method for that name exists.
+     * 返回方法定义，如果没有相应的方法存在，则返回 null
+     */
+    @Nullable
+    public abstract ServerMethodDefinition<?, ?> lookupMethod(String methodName, @Nullable String authority);
 
-  /**
-   * Lookup a {@link ServerMethodDefinition} by its fully-qualified name.
-   *
-   * @param methodName to lookup {@link ServerMethodDefinition} for.
-   * @return the resolved method or {@code null} if no method for that name exists.
-   */
-  @Nullable
-  public final ServerMethodDefinition<?, ?> lookupMethod(String methodName) {
-    return lookupMethod(methodName, null);
-  }
+    /**
+     * Lookup a {@link ServerMethodDefinition} by its fully-qualified name.
+     * 根据限定全名查找方法的定义
+     *
+     * @param methodName to lookup {@link ServerMethodDefinition} for.
+     *                   要查找的方法名称
+     * @return the resolved method or {@code null} if no method for that name exists.
+     * 返回方法定义，如果没有相应的方法存在，则返回 null
+     */
+    @Nullable
+    public final ServerMethodDefinition<?, ?> lookupMethod(String methodName) {
+        return lookupMethod(methodName, null);
+    }
 
 }
--- core/src/main/java/io/grpc/util/MutableHandlerRegistry.java
+++ core/src/main/java/io/grpc/util/MutableHandlerRegistry.java
@@ -22,16 +22,18 @@ import io.grpc.HandlerRegistry;
 import io.grpc.MethodDescriptor;
 import io.grpc.ServerMethodDefinition;
 import io.grpc.ServerServiceDefinition;
+
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.ThreadSafe;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
-import javax.annotation.Nullable;
-import javax.annotation.concurrent.ThreadSafe;
 
 /**
  * Default implementation of {@link MutableHandlerRegistry}.
+ * 处理器注册器的实现
  *
  * <p>Uses {@link ConcurrentHashMap} to avoid service registration excessively
  * blocking method lookup.
@@ -39,63 +41,77 @@ import javax.annotation.concurrent.ThreadSafe;
 @ThreadSafe
 @ExperimentalApi("https://github.com/grpc/grpc-java/issues/933")
 public final class MutableHandlerRegistry extends HandlerRegistry {
-  private final ConcurrentMap<String, ServerServiceDefinition> services
-      = new ConcurrentHashMap<>();
 
-  /**
-   * Registers a service.
-   *
-   * @return the previously registered service with the same service descriptor name if exists,
-   *         otherwise {@code null}.
-   */
-  @Nullable
-  public ServerServiceDefinition addService(ServerServiceDefinition service) {
-    return services.put(service.getServiceDescriptor().getName(), service);
-  }
+    private final ConcurrentMap<String, ServerServiceDefinition> services = new ConcurrentHashMap<>();
 
-  /**
-   * Registers a service.
-   *
-   * @return the previously registered service with the same service descriptor name if exists,
-   *         otherwise {@code null}.
-   */
-  @Nullable
-  public ServerServiceDefinition addService(BindableService bindableService) {
-    return addService(bindableService.bindService());
-  }
-
-  /**
-   * Removes a registered service
-   *
-   * @return true if the service was found to be removed.
-   */
-  public boolean removeService(ServerServiceDefinition service) {
-    return services.remove(service.getServiceDescriptor().getName(), service);
-  }
-
-  /**
-   *  Note: This does not necessarily return a consistent view of the map.
-   */
-  @Override
-  @ExperimentalApi("https://github.com/grpc/grpc-java/issues/2222")
-  public List<ServerServiceDefinition> getServices() {
-    return Collections.unmodifiableList(new ArrayList<>(services.values()));
-  }
-
-  /**
-   * Note: This does not actually honor the authority provided.  It will, eventually in the future.
-   */
-  @Override
-  @Nullable
-  public ServerMethodDefinition<?, ?> lookupMethod(String methodName, @Nullable String authority) {
-    String serviceName = MethodDescriptor.extractFullServiceName(methodName);
-    if (serviceName == null) {
-      return null;
+    /**
+     * Registers a service.
+     * 注册服务
+     *
+     * @return the previously registered service with the same service descriptor name if exists,
+     * otherwise {@code null}.
+     * 返回同一个服务定义之前注册的定义，如果没有则返回 null
+     */
+    @Nullable
+    public ServerServiceDefinition addService(ServerServiceDefinition service) {
+        return services.put(service.getServiceDescriptor().getName(), service);
     }
-    ServerServiceDefinition service = services.get(serviceName);
-    if (service == null) {
-      return null;
+
+    /**
+     * Registers a service.
+     * 注册服务
+     *
+     * @return the previously registered service with the same service descriptor name if exists,
+     * otherwise {@code null}.
+     * 返回同一个服务定义之前注册的定义，如果没有则返回 null
+     */
+    @Nullable
+    public ServerServiceDefinition addService(BindableService bindableService) {
+        return addService(bindableService.bindService());
     }
-    return service.getMethod(methodName);
-  }
+
+    /**
+     * Removes a registered service
+     * 移除注册的服务
+     *
+     * @return true if the service was found to be removed.
+     * 如果存在且被移除则返回 true
+     */
+    public boolean removeService(ServerServiceDefinition service) {
+        return services.remove(service.getServiceDescriptor().getName(), service);
+    }
+
+    /**
+     * Note: This does not necessarily return a consistent view of the map.
+     * 返回所有注册的服务
+     */
+    @Override
+    @ExperimentalApi("https://github.com/grpc/grpc-java/issues/2222")
+    public List<ServerServiceDefinition> getServices() {
+        return Collections.unmodifiableList(new ArrayList<>(services.values()));
+    }
+
+    /**
+     * Note: This does not actually honor the authority provided.  It will, eventually in the future.
+     * 根据方法限定名查找服务
+     */
+    @Override
+    @Nullable
+    public ServerMethodDefinition<?, ?> lookupMethod(String methodName, @Nullable String authority) {
+        // 从方法名中获取服务名
+        String serviceName = MethodDescriptor.extractFullServiceName(methodName);
+
+        // 如果没有则返回 null
+        if (serviceName == null) {
+            return null;
+        }
+
+        // 根据服务名从注册器中获取服务
+        ServerServiceDefinition service = services.get(serviceName);
+        if (service == null) {
+            return null;
+        }
+        // 根据方法名从服务中获取方法
+        return service.getMethod(methodName);
+    }
 }
