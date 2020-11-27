a2aa0107451847b7051dff0a55e531ab1c230fd5
helloworlde <hellowoodes@gmail.com>
2020-11-27 10:24:18 +0800

    CallStreamObserver

--- stub/src/main/java/io/grpc/stub/CallStreamObserver.java
+++ stub/src/main/java/io/grpc/stub/CallStreamObserver.java
@@ -21,6 +21,7 @@ import io.grpc.ExperimentalApi;
 /**
  * A refinement of StreamObserver provided by the GRPC runtime to the application (the client or
  * the server) that allows for more complex interactions with call behavior.
+ * gRPC 提供给应用的更细化的 StreamObserver，允许更复杂的相互调用行为
  *
  * <p>In any call there are logically four {@link StreamObserver} implementations:
  * <ul>
@@ -39,13 +40,21 @@ import io.grpc.ExperimentalApi;
  *   the server uses this {@code StreamObserver} to send messages (responses) to the client.
  *   </li>
  * </ul>
+ * <p>
+ * 任意类型的调用有四个实现：
+ * 1. 客户端的入站，从 Server 端接收到消息时调用，由客户端应用实现，并传递给 Stub 对象的服务方法
+ * 2. 客户端的出站，由 gRPC 库实现，提供给客户端程序使用 StreamObserver 发送消息给 Server
+ * 3. 服务端的入站，当服务端接收到客户端的消息时调用，由服务端应用实现，并从客户端流和双向流方法的实现返回
+ * 4. 服务端的出站，由 gRPC 库提供 StreamObserver 用于服务端发送消息给客户端
  *
  * <p>Implementations of this class represent the 'outbound' message streams. The client-side
  * one is {@link ClientCallStreamObserver} and the service-side one is
  * {@link ServerCallStreamObserver}.
+ * 这个类的实现代表出站的消息流，客户端的实现是 ClientCallStreamObserver，服务端的实现是 ServerCallStreamObserver
  *
  * <p>Like {@code StreamObserver}, implementations are not required to be thread-safe; if multiple
  * threads will be writing to an instance concurrently, the application must synchronize its calls.
+ * 和 StreamObserver 一样，实现不要求是线程安全的，如果有多个线程并发写入，应用应当确保同步调用
  *
  * <p>DO NOT MOCK: The API is too complex to reliably mock. Use InProcessChannelBuilder to create
  * "real" RPCs suitable for testing.
@@ -53,80 +62,102 @@ import io.grpc.ExperimentalApi;
 @ExperimentalApi("https://github.com/grpc/grpc-java/issues/1788")
 public abstract class CallStreamObserver<V> implements StreamObserver<V> {
 
-  /**
-   * If {@code true}, indicates that the observer is capable of sending additional messages
-   * without requiring excessive buffering internally. This value is just a suggestion and the
-   * application is free to ignore it, however doing so may result in excessive buffering within the
-   * observer.
-   *
-   * <p>If {@code false}, the runnable passed to {@link #setOnReadyHandler} will be called after
-   * {@code isReady()} transitions to {@code true}.
-   */
-  public abstract boolean isReady();
+    /**
+     * If {@code true}, indicates that the observer is capable of sending additional messages
+     * without requiring excessive buffering internally. This value is just a suggestion and the
+     * application is free to ignore it, however doing so may result in excessive buffering within the
+     * observer.
+     * 如果是 true，代表 observer 已经准备好发送附加的消息而不需要额外的内部缓冲，这仅仅是一个建议，应用可以忽略，
+     * 然而这样可能会导致 observer 有过多的缓冲
+     *
+     * <p>If {@code false}, the runnable passed to {@link #setOnReadyHandler} will be called after
+     * {@code isReady()} transitions to {@code true}.
+     * 如果是 false，传递给 setOnReadyHandler 的任务将会在 isReady 过渡为 true 后调用
+     */
+    public abstract boolean isReady();
 
-  /**
-   * Set a {@link Runnable} that will be executed every time the stream {@link #isReady()} state
-   * changes from {@code false} to {@code true}.  While it is not guaranteed that the same
-   * thread will always be used to execute the {@link Runnable}, it is guaranteed that executions
-   * are serialized with calls to the 'inbound' {@link StreamObserver}.
-   *
-   * <p>On client-side this method may only be called during {@link
-   * ClientResponseObserver#beforeStart}. On server-side it may only be called during the initial
-   * call to the application, before the service returns its {@code StreamObserver}.
-   *
-   * <p>Because there is a processing delay to deliver this notification, it is possible for
-   * concurrent writes to cause {@code isReady() == false} within this callback. Handle "spurious"
-   * notifications by checking {@code isReady()}'s current value instead of assuming it is now
-   * {@code true}. If {@code isReady() == false} the normal expectations apply, so there would be
-   * <em>another</em> {@code onReadyHandler} callback.
-   *
-   * @param onReadyHandler to call when peer is ready to receive more messages.
-   */
-  public abstract void setOnReadyHandler(Runnable onReadyHandler);
+    /**
+     * Set a {@link Runnable} that will be executed every time the stream {@link #isReady()} state
+     * changes from {@code false} to {@code true}.  While it is not guaranteed that the same
+     * thread will always be used to execute the {@link Runnable}, it is guaranteed that executions
+     * are serialized with calls to the 'inbound' {@link StreamObserver}.
+     * 设置一个每次 isReady 状态由 false 变为 true 后都会执行的任务，并不能保证任务总是由同一个线程执行，
+     * 但是可以保证可以通过调用入站 StreamObserver 来序列化执行
+     *
+     * <p>On client-side this method may only be called during {@link
+     * ClientResponseObserver#beforeStart}. On server-side it may only be called during the initial
+     * call to the application, before the service returns its {@code StreamObserver}.
+     * 在客户端，这个方法只有在 ClientResponseObserver#beforeStart 时会被调用，在 Server 端，只有在应用初始
+     * 化，在服务返回 StreamObserver 之前调用
+     *
+     * <p>Because there is a processing delay to deliver this notification, it is possible for
+     * concurrent writes to cause {@code isReady() == false} within this callback. Handle "spurious"
+     * notifications by checking {@code isReady()}'s current value instead of assuming it is now
+     * {@code true}. If {@code isReady() == false} the normal expectations apply, so there would be
+     * <em>another</em> {@code onReadyHandler} callback.
+     * 因为在投递通知时有处理延迟，在并发写入时可能会造成 isReady 变为 false，可以 isReady 状态而不是假定为 true，
+     * 如果 isReady 是 false 是正常期望，那么应当有另外一个 onReadyHandler 回调
+     *
+     * @param onReadyHandler to call when peer is ready to receive more messages.
+     *                       当连接准备好接收更多消息的时候调用
+     */
+    public abstract void setOnReadyHandler(Runnable onReadyHandler);
 
-  /**
-   * Disables automatic flow control where a token is returned to the peer after a call
-   * to the 'inbound' {@link io.grpc.stub.StreamObserver#onNext(Object)} has completed. If disabled
-   * an application must make explicit calls to {@link #request} to receive messages.
-   *
-   * <p>On client-side this method may only be called during {@link
-   * ClientResponseObserver#beforeStart}. On server-side it may only be called during the initial
-   * call to the application, before the service returns its {@code StreamObserver}.
-   *
-   * <p>Note that for cases where the runtime knows that only one inbound message is allowed
-   * calling this method will have no effect and the runtime will always permit one and only
-   * one message. This is true for:
-   * <ul>
-   *   <li>{@link io.grpc.MethodDescriptor.MethodType#UNARY} operations on both the
-   *   client and server.
-   *   </li>
-   *   <li>{@link io.grpc.MethodDescriptor.MethodType#CLIENT_STREAMING} operations on the client.
-   *   </li>
-   *   <li>{@link io.grpc.MethodDescriptor.MethodType#SERVER_STREAMING} operations on the server.
-   *   </li>
-   * </ul>
-   * </p>
-   * 
-   * <p>This API is being replaced, but is not yet deprecated. On server-side it being replaced
-   * with {@link ServerCallStreamObserver#disableAutoRequest}. On client-side {@link
-   * ClientCallStreamObserver#disableAutoRequestWithInitial disableAutoRequestWithInitial(1)}.
-   */
-  public abstract void disableAutoInboundFlowControl();
+    /**
+     * Disables automatic flow control where a token is returned to the peer after a call
+     * to the 'inbound' {@link io.grpc.stub.StreamObserver#onNext(Object)} has completed. If disabled
+     * an application must make explicit calls to {@link #request} to receive messages.
+     * 禁用自动流控，在该调用中，当入站的请求处理完成后，会返回 token 给对方，禁用之后，应用必须显式调用
+     * request 才能接收到消息
+     *
+     * <p>On client-side this method may only be called during {@link
+     * ClientResponseObserver#beforeStart}. On server-side it may only be called during the initial
+     * call to the application, before the service returns its {@code StreamObserver}.
+     * 在客户端这个方法只会在客户端启动时通过 ClientResponseObserver#beforeStart 调用，在服务端，只有
+     * 在应用初始化，返回服务的 StreamObserver 之前调用
+     *
+     * <p>Note that for cases where the runtime knows that only one inbound message is allowed
+     * calling this method will have no effect and the runtime will always permit one and only
+     * one message. This is true for:
+     * <ul>
+     *   <li>{@link io.grpc.MethodDescriptor.MethodType#UNARY} operations on both the
+     *   client and server.
+     *   </li>
+     *   <li>{@link io.grpc.MethodDescriptor.MethodType#CLIENT_STREAMING} operations on the client.
+     *   </li>
+     *   <li>{@link io.grpc.MethodDescriptor.MethodType#SERVER_STREAMING} operations on the server.
+     *   </li>
+     * </ul>
+     * </p>
+     * 注意，对于运行时知道仅允许一条入站消息调用此方法无效的情况，并且运行时将始终仅允许一条消息
+     * 1. 服务端和客户端的 UNARY 调用
+     * 2. 客户端的 CLIENT_STREAMING 调用
+     * 3. 服务端的 SERVER_STREAMING 调用
+     *
+     * <p>This API is being replaced, but is not yet deprecated. On server-side it being replaced
+     * with {@link ServerCallStreamObserver#disableAutoRequest}. On client-side {@link
+     * ClientCallStreamObserver#disableAutoRequestWithInitial disableAutoRequestWithInitial(1)}.
+     */
+    public abstract void disableAutoInboundFlowControl();
 
-  /**
-   * Requests the peer to produce {@code count} more messages to be delivered to the 'inbound'
-   * {@link StreamObserver}.
-   *
-   * <p>This method is safe to call from multiple threads without external synchronization.
-   *
-   * @param count more messages
-   */
-  public abstract void request(int count);
+    /**
+     * Requests the peer to produce {@code count} more messages to be delivered to the 'inbound'
+     * {@link StreamObserver}.
+     * 要求对等端产生指定数量的消息，投递给入站的 StreamObserver
+     *
+     * <p>This method is safe to call from multiple threads without external synchronization.
+     * 这个方法多次调用是安全的，不需要额外的同步
+     *
+     * @param count more messages
+     */
+    public abstract void request(int count);
 
-  /**
-   * Sets message compression for subsequent calls to {@link #onNext}.
-   *
-   * @param enable whether to enable compression.
-   */
-  public abstract void setMessageCompression(boolean enable);
+    /**
+     * Sets message compression for subsequent calls to {@link #onNext}.
+     * 为后续的调用设置消息压缩
+     *
+     * @param enable whether to enable compression.
+     *               是否开启压缩
+     */
+    public abstract void setMessageCompression(boolean enable);
 }
--- stub/src/main/java/io/grpc/stub/StreamObserver.java
+++ stub/src/main/java/io/grpc/stub/StreamObserver.java
@@ -18,12 +18,16 @@ package io.grpc.stub;
 
 /**
  * Receives notifications from an observable stream of messages.
+ * 从可观察的消息流中获取通知
  *
  * <p>It is used by both the client stubs and service implementations for sending or receiving
  * stream messages. It is used for all {@link io.grpc.MethodDescriptor.MethodType}, including
  * {@code UNARY} calls.  For outgoing messages, a {@code StreamObserver} is provided by the GRPC
  * library to the application. For incoming messages, the application implements the
  * {@code StreamObserver} and passes it to the GRPC library for receiving.
+ * 用于客户端 Stub 和 Server 端的服务实现发送或接收消息，被所有的 MethodType 使用，包括 Unary 调用，对于
+ * 出站的请求，StreamObserver 由 gRPC 库为应用提供，对于入站的请求，由应用实现 StreamObserver 并且传递
+ * 给 gRPC 库
  *
  * <p>Implementations are not required to be thread-safe (but should be
  * <a href="http://www.ibm.com/developerworks/library/j-jtp09263/">thread-compatible</a>).
@@ -31,49 +35,65 @@ package io.grpc.stub;
  * not need to be synchronized together; incoming and outgoing directions are independent.
  * Since individual {@code StreamObserver}s are not thread-safe, if multiple threads will be
  * writing to a {@code StreamObserver} concurrently, the application must synchronize calls.
+ * 并不一定要求实现是线程安全的，但要求是线程兼容的，单独的 StreamObserver 不需要一起同步
+ * 传入和传出的方向是独立的，因为独立的 StreamObserver 不是线程安全的，如果有多个线程并发写入，应用必须保证
+ * 同步调用
  */
-public interface StreamObserver<V>  {
-  /**
-   * Receives a value from the stream.
-   *
-   * <p>Can be called many times but is never called after {@link #onError(Throwable)} or {@link
-   * #onCompleted()} are called.
-   *
-   * <p>Unary calls must invoke onNext at most once.  Clients may invoke onNext at most once for
-   * server streaming calls, but may receive many onNext callbacks.  Servers may invoke onNext at
-   * most once for client streaming calls, but may receive many onNext callbacks.
-   *
-   * <p>If an exception is thrown by an implementation the caller is expected to terminate the
-   * stream by calling {@link #onError(Throwable)} with the caught exception prior to
-   * propagating it.
-   *
-   * @param value the value passed to the stream
-   */
-  void onNext(V value);
+public interface StreamObserver<V> {
+    /**
+     * Receives a value from the stream.
+     * 从流中接收值
+     *
+     * <p>Can be called many times but is never called after {@link #onError(Throwable)} or {@link
+     * #onCompleted()} are called.
+     * 可以被调用多次，但是在调用 onError 或 onComplete 之后不能再调用
+     *
+     * <p>Unary calls must invoke onNext at most once.  Clients may invoke onNext at most once for
+     * server streaming calls, but may receive many onNext callbacks.  Servers may invoke onNext at
+     * most once for client streaming calls, but may receive many onNext callbacks.
+     * Unary 调用最多只能调用一次 onNext；对于 Server 端流请求，客户端只能调用一次 onNext，但是可以接收多次
+     * onNext 回调；对于客户端的流请求，Server 只能调用一次 onNext，但是可以接收多次 onNext 回调
+     *
+     * <p>If an exception is thrown by an implementation the caller is expected to terminate the
+     * stream by calling {@link #onError(Throwable)} with the caught exception prior to
+     * propagating it.
+     * 如果实现中有抛出异常，则期望使用捕获的异常终止流
+     *
+     * @param value the value passed to the stream
+     *              传递给流的值
+     */
+    void onNext(V value);
 
-  /**
-   * Receives a terminating error from the stream.
-   *
-   * <p>May only be called once and if called it must be the last method called. In particular if an
-   * exception is thrown by an implementation of {@code onError} no further calls to any method are
-   * allowed.
-   *
-   * <p>{@code t} should be a {@link io.grpc.StatusException} or {@link
-   * io.grpc.StatusRuntimeException}, but other {@code Throwable} types are possible. Callers should
-   * generally convert from a {@link io.grpc.Status} via {@link io.grpc.Status#asException()} or
-   * {@link io.grpc.Status#asRuntimeException()}. Implementations should generally convert to a
-   * {@code Status} via {@link io.grpc.Status#fromThrowable(Throwable)}.
-   *
-   * @param t the error occurred on the stream
-   */
-  void onError(Throwable t);
+    /**
+     * Receives a terminating error from the stream.
+     * 从流中接收终止错误
+     *
+     * <p>May only be called once and if called it must be the last method called. In particular if an
+     * exception is thrown by an implementation of {@code onError} no further calls to any method are
+     * allowed.
+     * 只能调用一次，只能作为最后一个方法调用，特别是，如果调用 onError 触发了异常，则不允许有任何方法调用
+     *
+     * <p>{@code t} should be a {@link io.grpc.StatusException} or {@link
+     * io.grpc.StatusRuntimeException}, but other {@code Throwable} types are possible. Callers should
+     * generally convert from a {@link io.grpc.Status} via {@link io.grpc.Status#asException()} or
+     * {@link io.grpc.Status#asRuntimeException()}. Implementations should generally convert to a
+     * {@code Status} via {@link io.grpc.Status#fromThrowable(Throwable)}.
+     * 异常应当是 StatusException 或 StatusRuntimeException， 但是其他异常也有可能，调用者应当将状态通过
+     * asException 或者 asRuntimeException 转为异常；实现应当将异常转为状态
+     *
+     * @param t the error occurred on the stream
+     *          流中发生的错误
+     */
+    void onError(Throwable t);
 
-  /**
-   * Receives a notification of successful stream completion.
-   *
-   * <p>May only be called once and if called it must be the last method called. In particular if an
-   * exception is thrown by an implementation of {@code onCompleted} no further calls to any method
-   * are allowed.
-   */
-  void onCompleted();
+    /**
+     * Receives a notification of successful stream completion.
+     * 流成功完成的回调
+     *
+     * <p>May only be called once and if called it must be the last method called. In particular if an
+     * exception is thrown by an implementation of {@code onCompleted} no further calls to any method
+     * are allowed.
+     * 只能被调用一次，只能作为最后一个方法调用，如果 onCompleted 抛出异常，则不允许有任何方法调用
+     */
+    void onCompleted();
 }
