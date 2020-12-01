07537f2d5a2fd16465d15e339e8dca7eeb432773
helloworlde <hellowoodes@gmail.com>
2020-12-01 10:23:12 +0800

    RpcMethod

--- stub/src/main/java/io/grpc/stub/annotations/RpcMethod.java
+++ stub/src/main/java/io/grpc/stub/annotations/RpcMethod.java
@@ -17,6 +17,7 @@
 package io.grpc.stub.annotations;
 
 import io.grpc.MethodDescriptor;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -26,11 +27,14 @@ import java.lang.annotation.Target;
  * {@link RpcMethod} contains a limited subset of information about the RPC to assist
  * <a href="https://docs.oracle.com/javase/6/docs/api/javax/annotation/processing/Processor.html">
  * Java Annotation Processors.</a>
+ * RpcMethod 包含请求信息的有限子集
  *
  * <p>
- *   This annotation is used by the gRPC stub compiler to annotate {@link MethodDescriptor}
- *   getters.  Users should not annotate their own classes with this annotation.  Not all stubs may
- *   have this annotation, so consumers should not assume that it is present.
+ * This annotation is used by the gRPC stub compiler to annotate {@link MethodDescriptor}
+ * getters.  Users should not annotate their own classes with this annotation.  Not all stubs may
+ * have this annotation, so consumers should not assume that it is present.
+ * 这个注解用于 gRPC Stub 来修改 MethodDescriptor 的 get 方法，用户不应当使用这个注解，不是所有的 Stub
+ * 有这个注解，因此，消费者不应当认为有这个注解
  * </p>
  *
  * @since 1.14.0
@@ -39,32 +43,42 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 public @interface RpcMethod {
 
-  /**
-   * The fully qualified method name.  This should match the name as returned by
-   * {@link MethodDescriptor#generateFullMethodName(String, String)}.
-   */
-  String fullMethodName();
+    /**
+     * The fully qualified method name.  This should match the name as returned by
+     * {@link MethodDescriptor#generateFullMethodName(String, String)}.
+     * 方法全名，应当和 MethodDescriptor#generateFullMethodName 返回的结果一致
+     */
+    String fullMethodName();
 
-  /**
-   * The request type of the method.  The request type class should be assignable from (i.e.
-   * {@link Class#isAssignableFrom(Class)} the request type {@code ReqT} of the
-   * {@link MethodDescriptor}.  Additionally, if the request {@code MethodDescriptor.Marshaller}
-   * is a {@code MethodDescriptor.ReflectableMarshaller}, the request type should be assignable
-   * from {@code MethodDescriptor.ReflectableMarshaller#getMessageClass()}.
-   */
-  Class<?> requestType();
+    /**
+     * The request type of the method.  The request type class should be assignable from (i.e.
+     * {@link Class#isAssignableFrom(Class)} the request type {@code ReqT} of the
+     * {@link MethodDescriptor}.  Additionally, if the request {@code MethodDescriptor.Marshaller}
+     * is a {@code MethodDescriptor.ReflectableMarshaller}, the request type should be assignable
+     * from {@code MethodDescriptor.ReflectableMarshaller#getMessageClass()}.
+     * <p>
+     * 方法的请求类型，请求类型应当通过 MethodDescriptor 的 ReqT 分配，即 Class#isAssignableFrom(Class)，
+     * 如果 MethodDescriptor.Marshaller 是 MethodDescriptor.ReflectableMarshaller，请求类型应当从
+     * MethodDescriptor.ReflectableMarshaller#getMessageClass() 设置
+     */
+    Class<?> requestType();
 
-  /**
-   * The response type of the method.  The response type class should be assignable from (i.e.
-   * {@link Class#isAssignableFrom(Class)} the response type {@code RespT} of the
-   * {@link MethodDescriptor}.  Additionally, if the response {@code MethodDescriptor.Marshaller}
-   * is a {@code MethodDescriptor.ReflectableMarshaller}, the response type should be assignable
-   * from {@code MethodDescriptor.ReflectableMarshaller#getMessageClass()}.
-   */
-  Class<?> responseType();
+    /**
+     * The response type of the method.  The response type class should be assignable from (i.e.
+     * {@link Class#isAssignableFrom(Class)} the response type {@code RespT} of the
+     * {@link MethodDescriptor}.  Additionally, if the response {@code MethodDescriptor.Marshaller}
+     * is a {@code MethodDescriptor.ReflectableMarshaller}, the response type should be assignable
+     * from {@code MethodDescriptor.ReflectableMarshaller#getMessageClass()}.
+     * <p>
+     * 响应类型，响应类型应当通过 MethodDescriptor 的 RespT 分配，即 Class#isAssignableFrom(Class)，
+     * 如果 MethodDescriptor.Marshaller 是 MethodDescriptor.ReflectableMarshaller，响应类型应当从
+     * MethodDescriptor.ReflectableMarshaller#getMessageClass() 设置
+     */
+    Class<?> responseType();
 
-  /**
-   * The call type of the method.
-   */
-  MethodDescriptor.MethodType methodType();
+    /**
+     * The call type of the method.
+     * 方法的调用类型
+     */
+    MethodDescriptor.MethodType methodType();
 }
