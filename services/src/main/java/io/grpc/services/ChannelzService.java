cb58e4a2bfb8e322cbf6e75f274b869b8e0acf9d
helloworlde <hellowoodes@gmail.com>
2021-01-07 10:27:07 +0800

    Channelz

--- services/src/main/java/io/grpc/services/ChannelzService.java
+++ services/src/main/java/io/grpc/services/ChannelzService.java
@@ -43,162 +43,182 @@ import io.grpc.stub.StreamObserver;
 
 /**
  * The channelz service provides stats about a running gRPC process.
+ * 用于提供运行汇中的 gRPC 进程统计的 Channelz
  */
 @ExperimentalApi("https://github.com/grpc/grpc-java/issues/4206")
 public final class ChannelzService extends ChannelzGrpc.ChannelzImplBase {
-  private final InternalChannelz channelz;
-  private final int maxPageSize;
 
-  /**
-   * Creates an instance.
-   */
-  public static ChannelzService newInstance(int maxPageSize) {
-    return new ChannelzService(InternalChannelz.instance(), maxPageSize);
-  }
+    private final InternalChannelz channelz;
 
-  @VisibleForTesting
-  ChannelzService(InternalChannelz channelz, int maxPageSize) {
-    this.channelz = channelz;
-    this.maxPageSize = maxPageSize;
-  }
+    private final int maxPageSize;
 
-  /** Returns top level channel aka {@link io.grpc.ManagedChannel}. */
-  @Override
-  public void getTopChannels(
-      GetTopChannelsRequest request, StreamObserver<GetTopChannelsResponse> responseObserver) {
-    InternalChannelz.RootChannelList rootChannels
-        = channelz.getRootChannels(request.getStartChannelId(), maxPageSize);
-
-    GetTopChannelsResponse resp;
-    try {
-      resp = ChannelzProtoUtil.toGetTopChannelResponse(rootChannels);
-    } catch (StatusRuntimeException e) {
-      responseObserver.onError(e);
-      return;
+    /**
+     * Creates an instance.
+     * 创建实例
+     */
+    public static ChannelzService newInstance(int maxPageSize) {
+        return new ChannelzService(InternalChannelz.instance(), maxPageSize);
     }
 
-    responseObserver.onNext(resp);
-    responseObserver.onCompleted();
-  }
-
-  /** Returns a top level channel aka {@link io.grpc.ManagedChannel}. */
-  @Override
-  public void getChannel(
-      GetChannelRequest request, StreamObserver<GetChannelResponse> responseObserver) {
-    InternalInstrumented<ChannelStats> s = channelz.getRootChannel(request.getChannelId());
-    if (s == null) {
-      responseObserver.onError(
-          Status.NOT_FOUND.withDescription("Can't find channel " + request.getChannelId())
-              .asRuntimeException());
-      return;
+    @VisibleForTesting
+    ChannelzService(InternalChannelz channelz, int maxPageSize) {
+        this.channelz = channelz;
+        this.maxPageSize = maxPageSize;
     }
 
-    GetChannelResponse resp;
-    try {
-      resp = GetChannelResponse
-          .newBuilder()
-          .setChannel(ChannelzProtoUtil.toChannel(s))
-          .build();
-    } catch (StatusRuntimeException e) {
-      responseObserver.onError(e);
-      return;
+    /**
+     * Returns top level channel aka {@link io.grpc.ManagedChannel}.
+     * 返回 ManagedChannel
+     */
+    @Override
+    public void getTopChannels(GetTopChannelsRequest request,
+                               StreamObserver<GetTopChannelsResponse> responseObserver) {
+
+        // 获取根 Channel
+        InternalChannelz.RootChannelList rootChannels = channelz.getRootChannels(request.getStartChannelId(), maxPageSize);
+
+        GetTopChannelsResponse resp;
+        try {
+            // 将 Channel 转为响应结果
+            resp = ChannelzProtoUtil.toGetTopChannelResponse(rootChannels);
+        } catch (StatusRuntimeException e) {
+            responseObserver.onError(e);
+            return;
+        }
+
+        responseObserver.onNext(resp);
+        responseObserver.onCompleted();
     }
 
-    responseObserver.onNext(resp);
-    responseObserver.onCompleted();
-  }
+    /**
+     * Returns a top level channel aka {@link io.grpc.ManagedChannel}.
+     * 根据 ID 获取 Channel
+     */
+    @Override
+    public void getChannel(GetChannelRequest request,
+                           StreamObserver<GetChannelResponse> responseObserver) {
+        InternalInstrumented<ChannelStats> s = channelz.getRootChannel(request.getChannelId());
+        if (s == null) {
+            responseObserver.onError(Status.NOT_FOUND.withDescription("Can't find channel " + request.getChannelId()).asRuntimeException());
+            return;
+        }
 
-  /** Returns servers. */
-  @Override
-  public void getServers(
-      GetServersRequest request, StreamObserver<GetServersResponse> responseObserver) {
-    ServerList servers = channelz.getServers(request.getStartServerId(), maxPageSize);
+        GetChannelResponse resp;
+        try {
+            resp = GetChannelResponse.newBuilder()
+                                     .setChannel(ChannelzProtoUtil.toChannel(s))
+                                     .build();
+        } catch (StatusRuntimeException e) {
+            responseObserver.onError(e);
+            return;
+        }
 
-    GetServersResponse resp;
-    try {
-      resp = ChannelzProtoUtil.toGetServersResponse(servers);
-    } catch (StatusRuntimeException e) {
-      responseObserver.onError(e);
-      return;
+        responseObserver.onNext(resp);
+        responseObserver.onCompleted();
     }
 
-    responseObserver.onNext(resp);
-    responseObserver.onCompleted();
-  }
+    /**
+     * Returns servers.
+     * 返回 Server 集合
+     */
+    @Override
+    public void getServers(GetServersRequest request,
+                           StreamObserver<GetServersResponse> responseObserver) {
+        ServerList servers = channelz.getServers(request.getStartServerId(), maxPageSize);
 
-  /** Returns a subchannel. */
-  @Override
-  public void getSubchannel(
-      GetSubchannelRequest request, StreamObserver<GetSubchannelResponse> responseObserver) {
-    InternalInstrumented<ChannelStats> s = channelz.getSubchannel(request.getSubchannelId());
-    if (s == null) {
-      responseObserver.onError(
-          Status.NOT_FOUND.withDescription("Can't find subchannel " + request.getSubchannelId())
-              .asRuntimeException());
-      return;
+        GetServersResponse resp;
+        try {
+            resp = ChannelzProtoUtil.toGetServersResponse(servers);
+        } catch (StatusRuntimeException e) {
+            responseObserver.onError(e);
+            return;
+        }
+
+        responseObserver.onNext(resp);
+        responseObserver.onCompleted();
     }
 
-    GetSubchannelResponse resp;
-    try {
-      resp = GetSubchannelResponse
-          .newBuilder()
-          .setSubchannel(ChannelzProtoUtil.toSubchannel(s))
-          .build();
-    } catch (StatusRuntimeException e) {
-      responseObserver.onError(e);
-      return;
+    /**
+     * Returns a subchannel.
+     * 根据 Subchannel ID 获取 Subchannel
+     */
+    @Override
+    public void getSubchannel(GetSubchannelRequest request,
+                              StreamObserver<GetSubchannelResponse> responseObserver) {
+
+        InternalInstrumented<ChannelStats> s = channelz.getSubchannel(request.getSubchannelId());
+        if (s == null) {
+            responseObserver.onError(Status.NOT_FOUND.withDescription("Can't find subchannel " + request.getSubchannelId())
+                                                     .asRuntimeException());
+            return;
+        }
+
+        GetSubchannelResponse resp;
+        try {
+            resp = GetSubchannelResponse.newBuilder()
+                                        .setSubchannel(ChannelzProtoUtil.toSubchannel(s))
+                                        .build();
+        } catch (StatusRuntimeException e) {
+            responseObserver.onError(e);
+            return;
+        }
+
+        responseObserver.onNext(resp);
+        responseObserver.onCompleted();
     }
 
-    responseObserver.onNext(resp);
-    responseObserver.onCompleted();
-  }
+    /**
+     * Returns a socket.
+     * 根据 Socket ID 获取 Socket
+     */
+    @Override
+    public void getSocket(GetSocketRequest request,
+                          StreamObserver<GetSocketResponse> responseObserver) {
 
-  /** Returns a socket. */
-  @Override
-  public void getSocket(
-      GetSocketRequest request, StreamObserver<GetSocketResponse> responseObserver) {
-    InternalInstrumented<SocketStats> s = channelz.getSocket(request.getSocketId());
-    if (s == null) {
-      responseObserver.onError(
-          Status.NOT_FOUND.withDescription("Can't find socket " + request.getSocketId())
-              .asRuntimeException());
-      return;
+        InternalInstrumented<SocketStats> s = channelz.getSocket(request.getSocketId());
+        if (s == null) {
+            responseObserver.onError(Status.NOT_FOUND.withDescription("Can't find socket " + request.getSocketId())
+                                                     .asRuntimeException());
+            return;
+        }
+
+        GetSocketResponse resp;
+        try {
+            resp = GetSocketResponse.newBuilder()
+                                    .setSocket(ChannelzProtoUtil.toSocket(s))
+                                    .build();
+        } catch (StatusRuntimeException e) {
+            responseObserver.onError(e);
+            return;
+        }
+
+        responseObserver.onNext(resp);
+        responseObserver.onCompleted();
     }
 
-    GetSocketResponse resp;
-    try {
-      resp =
-          GetSocketResponse.newBuilder().setSocket(ChannelzProtoUtil.toSocket(s)).build();
-    } catch (StatusRuntimeException e) {
-      responseObserver.onError(e);
-      return;
+    /**
+     * 根据 Server ID 获取所有的 Socket
+     */
+    @Override
+    public void getServerSockets(GetServerSocketsRequest request,
+                                 StreamObserver<GetServerSocketsResponse> responseObserver) {
+
+        ServerSocketsList serverSockets = channelz.getServerSockets(request.getServerId(), request.getStartSocketId(), maxPageSize);
+        if (serverSockets == null) {
+            responseObserver.onError(Status.NOT_FOUND.withDescription("Can't find server " + request.getServerId())
+                                                     .asRuntimeException());
+            return;
+        }
+
+        GetServerSocketsResponse resp;
+        try {
+            resp = ChannelzProtoUtil.toGetServerSocketsResponse(serverSockets);
+        } catch (StatusRuntimeException e) {
+            responseObserver.onError(e);
+            return;
+        }
+
+        responseObserver.onNext(resp);
+        responseObserver.onCompleted();
     }
-
-    responseObserver.onNext(resp);
-    responseObserver.onCompleted();
-  }
-
-  @Override
-  public void getServerSockets(
-      GetServerSocketsRequest request, StreamObserver<GetServerSocketsResponse> responseObserver) {
-    ServerSocketsList serverSockets
-        = channelz.getServerSockets(request.getServerId(), request.getStartSocketId(), maxPageSize);
-    if (serverSockets == null) {
-      responseObserver.onError(
-          Status.NOT_FOUND.withDescription("Can't find server " + request.getServerId())
-              .asRuntimeException());
-      return;
-    }
-
-    GetServerSocketsResponse resp;
-    try {
-      resp = ChannelzProtoUtil.toGetServerSocketsResponse(serverSockets);
-    } catch (StatusRuntimeException e) {
-      responseObserver.onError(e);
-      return;
-    }
-
-    responseObserver.onNext(resp);
-    responseObserver.onCompleted();
-  }
 }
