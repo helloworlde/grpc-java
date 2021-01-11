a7f401ee684d886c73aba5bef40f0f9858539dbc
helloworlde <hellowoodes@gmail.com>
2021-01-11 10:36:03 +0800

    Format code

--- api/src/main/java/io/grpc/SynchronizationContext.java
+++ api/src/main/java/io/grpc/SynchronizationContext.java
@@ -59,165 +59,164 @@ import static com.google.common.base.Preconditions.checkState;
 @ThreadSafe
 @ExperimentalApi("https://github.com/grpc/grpc-java/issues/4984")
 public final class SynchronizationContext implements Executor {
-  private final UncaughtExceptionHandler uncaughtExceptionHandler;
+    private final UncaughtExceptionHandler uncaughtExceptionHandler;
 
-  private final Queue<Runnable> queue = new ConcurrentLinkedQueue<>();
-  private final AtomicReference<Thread> drainingThread = new AtomicReference<>();
+    private final Queue<Runnable> queue = new ConcurrentLinkedQueue<>();
+    private final AtomicReference<Thread> drainingThread = new AtomicReference<>();
 
-  /**
-   * Creates a SynchronizationContext.
-   *
-   * @param uncaughtExceptionHandler handles exceptions thrown out of the tasks.  Different from
-   *        what's documented on {@link UncaughtExceptionHandler#uncaughtException}, the thread is
-   *        not terminated when the handler is called.
-   */
-  public SynchronizationContext(UncaughtExceptionHandler uncaughtExceptionHandler) {
-    this.uncaughtExceptionHandler =
-        checkNotNull(uncaughtExceptionHandler, "uncaughtExceptionHandler");
-  }
+    /**
+     * Creates a SynchronizationContext.
+     *
+     * @param uncaughtExceptionHandler handles exceptions thrown out of the tasks.  Different from
+     *                                 what's documented on {@link UncaughtExceptionHandler#uncaughtException}, the thread is
+     *                                 not terminated when the handler is called.
+     */
+    public SynchronizationContext(UncaughtExceptionHandler uncaughtExceptionHandler) {
+        this.uncaughtExceptionHandler =
+                checkNotNull(uncaughtExceptionHandler, "uncaughtExceptionHandler");
+    }
 
-  /**
-   * Run all tasks in the queue in the current thread, if no other thread is running this method.
-   * Otherwise do nothing.
-   *
-   * <p>Upon returning, it guarantees that all tasks submitted by {@code #executeLater} before it
-   * have been or will eventually be run, while not requiring any more calls to {@code drain()}.
-   */
-  public final void drain() {
-    do {
-      if (!drainingThread.compareAndSet(null, Thread.currentThread())) {
-        return;
-      }
-      try {
-        Runnable runnable;
-        while ((runnable = queue.poll()) != null) {
-          try {
-            runnable.run();
-          } catch (Throwable t) {
-            uncaughtExceptionHandler.uncaughtException(Thread.currentThread(), t);
-          }
+    /**
+     * Run all tasks in the queue in the current thread, if no other thread is running this method.
+     * Otherwise do nothing.
+     *
+     * <p>Upon returning, it guarantees that all tasks submitted by {@code #executeLater} before it
+     * have been or will eventually be run, while not requiring any more calls to {@code drain()}.
+     */
+    public final void drain() {
+        do {
+            if (!drainingThread.compareAndSet(null, Thread.currentThread())) {
+                return;
+            }
+            try {
+                Runnable runnable;
+                while ((runnable = queue.poll()) != null) {
+                    try {
+                        runnable.run();
+                    } catch (Throwable t) {
+                        uncaughtExceptionHandler.uncaughtException(Thread.currentThread(), t);
+                    }
+                }
+            } finally {
+                drainingThread.set(null);
+            }
+            // must check queue again here to catch any added prior to clearing drainingThread
+        } while (!queue.isEmpty());
+    }
+
+    /**
+     * TODO 将请求添加到队列中
+     * Adds a task that will be run when {@link #drain} is called.
+     *
+     * <p>This is useful for cases where you want to enqueue a task while under a lock of your own,
+     * but don't want the tasks to be run under your lock (for fear of deadlock).  You can call {@link
+     * #executeLater} in the lock, and call {@link #drain} outside the lock.
+     */
+    public final void executeLater(Runnable runnable) {
+        queue.add(checkNotNull(runnable, "runnable is null"));
+    }
+
+    /**
+     * Adds a task and run it in this synchronization context as soon as possible.  The task may run
+     * inline.  If there are tasks that are previously queued by {@link #executeLater} but have not
+     * been run, this method will trigger them to be run before the given task.  This is equivalent to
+     * calling {@link #executeLater} immediately followed by {@link #drain}.
+     */
+    @Override
+    public final void execute(Runnable task) {
+        executeLater(task);
+        drain();
+    }
+
+    /**
+     * Throw {@link IllegalStateException} if this method is not called from this synchronization
+     * context.
+     */
+    public void throwIfNotInThisSynchronizationContext() {
+        checkState(Thread.currentThread() == drainingThread.get(),
+                "Not called from the SynchronizationContext");
+    }
+
+    /**
+     * Schedules a task to be added and run via {@link #execute} after a delay.
+     *
+     * @param task         the task being scheduled
+     * @param delay        the delay
+     * @param unit         the time unit for the delay
+     * @param timerService the {@code ScheduledExecutorService} that provides delayed execution
+     * @return an object for checking the status and/or cancel the scheduled task
+     */
+    public final ScheduledHandle schedule(
+            final Runnable task, long delay, TimeUnit unit, ScheduledExecutorService timerService) {
+        final ManagedRunnable runnable = new ManagedRunnable(task);
+        ScheduledFuture<?> future = timerService.schedule(new Runnable() {
+            @Override
+            public void run() {
+                execute(runnable);
+            }
+
+            @Override
+            public String toString() {
+                return task.toString() + "(scheduled in SynchronizationContext)";
+            }
+        }, delay, unit);
+        return new ScheduledHandle(runnable, future);
+    }
+
+    private static class ManagedRunnable implements Runnable {
+        final Runnable task;
+        boolean isCancelled;
+        boolean hasStarted;
+
+        ManagedRunnable(Runnable task) {
+            this.task = checkNotNull(task, "task");
         }
-      } finally {
-        drainingThread.set(null);
-      }
-      // must check queue again here to catch any added prior to clearing drainingThread
-    } while (!queue.isEmpty());
-  }
 
-  /**
-   * TODO 将请求添加到队列中
-   * Adds a task that will be run when {@link #drain} is called.
-   *
-   * <p>This is useful for cases where you want to enqueue a task while under a lock of your own,
-   * but don't want the tasks to be run under your lock (for fear of deadlock).  You can call {@link
-   * #executeLater} in the lock, and call {@link #drain} outside the lock.
-   */
-  public final void executeLater(Runnable runnable) {
-    queue.add(checkNotNull(runnable, "runnable is null"));
-  }
-
-  /**
-   * Adds a task and run it in this synchronization context as soon as possible.  The task may run
-   * inline.  If there are tasks that are previously queued by {@link #executeLater} but have not
-   * been run, this method will trigger them to be run before the given task.  This is equivalent to
-   * calling {@link #executeLater} immediately followed by {@link #drain}.
-   */
-  @Override
-  public final void execute(Runnable task) {
-    executeLater(task);
-    drain();
-  }
-
-  /**
-   * Throw {@link IllegalStateException} if this method is not called from this synchronization
-   * context.
-   */
-  public void throwIfNotInThisSynchronizationContext() {
-    checkState(Thread.currentThread() == drainingThread.get(),
-        "Not called from the SynchronizationContext");
-  }
-
-  /**
-   * Schedules a task to be added and run via {@link #execute} after a delay.
-   *
-   * @param task the task being scheduled
-   * @param delay the delay
-   * @param unit the time unit for the delay
-   * @param timerService the {@code ScheduledExecutorService} that provides delayed execution
-   *
-   * @return an object for checking the status and/or cancel the scheduled task
-   */
-  public final ScheduledHandle schedule(
-      final Runnable task, long delay, TimeUnit unit, ScheduledExecutorService timerService) {
-    final ManagedRunnable runnable = new ManagedRunnable(task);
-    ScheduledFuture<?> future = timerService.schedule(new Runnable() {
         @Override
         public void run() {
-          execute(runnable);
+            // The task may have been cancelled after timerService calls SynchronizationContext.execute()
+            // but before the runnable is actually run.  We must guarantee that the task will not be run
+            // in this case.
+            if (!isCancelled) {
+                hasStarted = true;
+                task.run();
+            }
         }
-
-        @Override
-        public String toString() {
-          return task.toString() + "(scheduled in SynchronizationContext)";
-        }
-      }, delay, unit);
-    return new ScheduledHandle(runnable, future);
-  }
-
-  private static class ManagedRunnable implements Runnable {
-    final Runnable task;
-    boolean isCancelled;
-    boolean hasStarted;
-
-    ManagedRunnable(Runnable task) {
-      this.task = checkNotNull(task, "task");
-    }
-
-    @Override
-    public void run() {
-      // The task may have been cancelled after timerService calls SynchronizationContext.execute()
-      // but before the runnable is actually run.  We must guarantee that the task will not be run
-      // in this case.
-      if (!isCancelled) {
-        hasStarted = true;
-        task.run();
-      }
-    }
-  }
-
-  /**
-   * Allows the user to check the status and/or cancel a task scheduled by {@link #schedule}.
-   * 允许用户检查任务的状态或者取消任务调度
-   *
-   * <p>This class is NOT thread-safe.  All methods must be run from the same {@link
-   * SynchronizationContext} as which the task was scheduled in.
-   * 这个类不是线程安全的，所有的方法必须通过调度任务的同一个 SynchronizationContext 访问
-   */
-  public static final class ScheduledHandle {
-    private final ManagedRunnable runnable;
-    private final ScheduledFuture<?> future;
-
-    private ScheduledHandle(ManagedRunnable runnable, ScheduledFuture<?> future) {
-      this.runnable = checkNotNull(runnable, "runnable");
-      this.future = checkNotNull(future, "future");
     }
 
     /**
-     * Cancel the task if it's still {@link #isPending pending}.
-     * 如果再等待，则取消任务
+     * Allows the user to check the status and/or cancel a task scheduled by {@link #schedule}.
+     * 允许用户检查任务的状态或者取消任务调度
+     *
+     * <p>This class is NOT thread-safe.  All methods must be run from the same {@link
+     * SynchronizationContext} as which the task was scheduled in.
+     * 这个类不是线程安全的，所有的方法必须通过调度任务的同一个 SynchronizationContext 访问
      */
-    public void cancel() {
-      runnable.isCancelled = true;
-      future.cancel(false);
-    }
+    public static final class ScheduledHandle {
+        private final ManagedRunnable runnable;
+        private final ScheduledFuture<?> future;
 
-    /**
-     * Returns {@code true} if the task will eventually run, meaning that it has neither started
-     * running nor been cancelled.
-     * 如果任务最终会执行，则返回 true，意味着既不是 started 也不是 cancelled 状态
-     */
-    public boolean isPending() {
-      return !(runnable.hasStarted || runnable.isCancelled);
+        private ScheduledHandle(ManagedRunnable runnable, ScheduledFuture<?> future) {
+            this.runnable = checkNotNull(runnable, "runnable");
+            this.future = checkNotNull(future, "future");
+        }
+
+        /**
+         * Cancel the task if it's still {@link #isPending pending}.
+         * 如果再等待，则取消任务
+         */
+        public void cancel() {
+            runnable.isCancelled = true;
+            future.cancel(false);
+        }
+
+        /**
+         * Returns {@code true} if the task will eventually run, meaning that it has neither started
+         * running nor been cancelled.
+         * 如果任务最终会执行，则返回 true，意味着既不是 started 也不是 cancelled 状态
+         */
+        public boolean isPending() {
+            return !(runnable.hasStarted || runnable.isCancelled);
+        }
     }
-  }
 }
